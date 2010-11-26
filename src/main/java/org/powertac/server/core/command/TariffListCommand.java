2499aa09478896163edb758bf5c1078ad9a60dbb
dschnurr <schnurr@iism.uni-karlsruhe.de>
2010-11-26 09:51:36 +0100

    Added command objects

---
+++ src/main/java/org/powertac/server/core/command/TariffListCommand.java
@@ -0,0 +1,11 @@
+package org.powertac.server.core.command;
+
+import org.powertac.server.core.domain.Tariff;
+
+import java.util.ArrayList;
+
+public class TariffListCommand {
+
+    ArrayList<Tariff> tariffs = new ArrayList<Tariff>();
+    
+}
