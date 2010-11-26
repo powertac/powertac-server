c72ac1a19a5ef64e7f1093b4cc0361dfd1323b50
dschnurr <schnurr@iism.uni-karlsruhe.de>
2010-11-26 09:51:10 +0100

    Basic methods for ACS

--- src/main/java/org/powertac/server/core/service/AccountingService.java
+++ src/main/java/org/powertac/server/core/service/AccountingService.java
@@ -2,14 +2,30 @@ package org.powertac.server.core.service;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.powertac.server.core.command.PublishTariffCommand;
 import org.powertac.server.core.command.RevokeTariffCommand;
+import org.powertac.server.core.command.UpdateTariffCommand;
 
 public class AccountingService {
 
-  private final Log log = LogFactory.getLog(AccountingService.class);
+    private final Log log = LogFactory.getLog(AccountingService.class);
 
-  public Boolean revokeTariff(RevokeTariffCommand rtc) {
-    log.error ("Revoke tariff " + rtc.getTariffId() + " of broker " + rtc.getAuthToken());
-    return true;
-  }
+    public Boolean publishTariff(PublishTariffCommand ptc) {
+        //Todo database create tariff entry
+        log.error("Publish tariff " + ptc.getTariffId() + " of broker " + ptc.getAuthToken());
+        return true;
+    }
+
+    public Boolean revokeTariff(RevokeTariffCommand rtc) {
+        //Todo database delete tariff entry
+        log.error("Revoke tariff " + rtc.getTariffId() + " of broker " + rtc.getAuthToken());
+        return true;
+    }
+
+    public Boolean updateTariff(UpdateTariffCommand utc) {
+        //Todo database update tariff entry
+        log.error("Update tariff " + utc.getTariffId() + " of broker " + utc.getAuthToken());
+        return true;
+    }
+
 }
