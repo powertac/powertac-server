d90dd8f28d5f0f1490b6f0f309e32a3237fc0001
Carsten Block <mail@carstenblock.org>
2010-11-26 10:49:02 +0100

    New Enumerations added

---
+++ src/main/java/org/powertac/server/core/enumeration/CustomerType.java
@@ -0,0 +1,15 @@
+package org.powertac.server.core.enumeration;
+
+public enum CustomerType {
+  ConsumerHousehold,
+  ConsumerOffice,
+  ConsumerFactory,
+  ConsumerElectricVehicle,
+  ConsumerOther,
+  ProducerSolar,
+  ProducerWind,
+  ProducerRunOfRiver,
+  ProducerPumpedStorage,
+  ProducerCHP,
+  ProducerFossil;
+}
