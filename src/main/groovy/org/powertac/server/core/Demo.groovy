ea8dc6bf941213b9dea1311a41e71b6a95d41a71
Carsten Block <mail@carstenblock.org>
2010-11-26 08:40:11 +0100

    Groovy Demo added.

--- src/main/groovy/org/powertac/server/core/Demo.groovy
+++
@@ -1,7 +0,0 @@
-package org.powertac.server.core
-
-class Demo {
-  public static void main(String[] args) {
-    println "Just a demo to show that groovy also works."
-  }
-}
---
+++ src/main/groovy/test/Demo.groovy
@@ -0,0 +1,8 @@
+package test
+
+
+class Demo {
+  public static void main(String[] args) {
+    println "This is a groovy demo"
+  }
+}
