f944163c5c431e1d5c252a5e870a88ed57a0f14f
unknown <flath@.fzi.de>
2010-11-26 11:57:00 +0100

    Signed-off-by: unknown <flath@.fzi.de>

--- src/main/java/org/powertac/server/core/domain/Tariff.java
+++ src/main/java/org/powertac/server/core/domain/Tariff.java
@@ -1,6 +1,7 @@
 package org.powertac.server.core.domain;
 
 import org.joda.time.LocalDateTime;
+import org.powertac.server.core.enumeration.TariffState;
 import org.springframework.roo.addon.entity.RooEntity;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.tostring.RooToString;
@@ -15,14 +16,6 @@ import java.math.BigDecimal;
  * @since 0.8-Server
  */
 public class Tariff {
-  public enum TariffState {
-    Published, //Tariff is publicly visible to brokers & consumers
-    Revoked,                          //Previously published tariff is revoked by broker -> no longer visible, no longer subscribable / negotiatiable to new customers
-    InNegotiation,                    //Specific tariff instance is currently (and privately) in negotiation between one customer and one broker
-    Subscribed,                       //Specific tariff instance which one broker and one customer agreed upon (might be running already startDate <= now) or in future (startDate > now)
-    Finished,                         //Tariff endDate < now; tariff is no longer valid and only kept in history
-    NegotiationAborted
-  }               //
 
   Competition competition;
   private Long tariffId;
