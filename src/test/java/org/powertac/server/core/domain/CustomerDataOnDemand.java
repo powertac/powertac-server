50f0aa01a45098ba7c81642d4c1a98b4abef934c
Carsten Block <mail@carstenblock.org>
2010-11-26 10:22:09 +0100

    Customer domain class tests added

---
+++ src/test/java/org/powertac/server/core/domain/CustomerDataOnDemand.java
@@ -0,0 +1,8 @@
+package org.powertac.server.core.domain;
+
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+import org.powertac.server.core.domain.Customer;
+
+@RooDataOnDemand(entity = Customer.class)
+public class CustomerDataOnDemand {
+}
---
+++ src/test/java/org/powertac/server/core/domain/CustomerIntegrationTest.java
@@ -0,0 +1,13 @@
+package org.powertac.server.core.domain;
+
+import org.springframework.roo.addon.test.RooIntegrationTest;
+import org.powertac.server.core.domain.Customer;
+import org.junit.Test;
+
+@RooIntegrationTest(entity = Customer.class)
+public class CustomerIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
