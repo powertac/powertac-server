e91986c4c28ae01af86acbb73dbbe7126545a1c5
Carsten Block <mail@carstenblock.org>
2010-11-26 10:21:31 +0100

    CashPosition tests added

---
+++ src/test/java/org/powertac/server/core/domain/CashPositionDataOnDemand.java
@@ -0,0 +1,8 @@
+package org.powertac.server.core.domain;
+
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+import org.powertac.server.core.domain.CashPosition;
+
+@RooDataOnDemand(entity = CashPosition.class)
+public class CashPositionDataOnDemand {
+}
---
+++ src/test/java/org/powertac/server/core/domain/CashPositionIntegrationTest.java
@@ -0,0 +1,13 @@
+package org.powertac.server.core.domain;
+
+import org.springframework.roo.addon.test.RooIntegrationTest;
+import org.powertac.server.core.domain.CashPosition;
+import org.junit.Test;
+
+@RooIntegrationTest(entity = CashPosition.class)
+public class CashPositionIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
