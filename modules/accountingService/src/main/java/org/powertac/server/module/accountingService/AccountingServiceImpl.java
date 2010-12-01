65986e9f6966b5fcff1960eb8a5e7b7123ce6b4b
ddauer <ddauer@gmail.com>
2010-12-01 21:48:26 +0100

    Added Accounting Service Module
    Added accounting service module and hooked it up to the server config

---
+++ modules/accountingService/src/main/java/org/powertac/server/module/accountingService/AccountingServiceImpl.java
@@ -0,0 +1,43 @@
+package org.powertac.server.module.accountingService;
+
+import org.powertac.common.commands.*;
+import org.powertac.common.interfaces.AccountingService;
+
+import java.util.List;
+
+public class AccountingServiceImpl implements AccountingService {
+
+    public AccountingServiceImpl() {
+        System.out.println("AccountingServiceImpl");
+    }
+
+    @Override
+    public List<MeterReadingBalanceCommand> processMeterReadings(List<MeterReadingCommand> meterReadingCommandList) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public DepotPositionCommand processDepotUpdate(DepotUpdateCommand depotUpdateCommand) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public CashPositionCommand processCashUpdate(CashUpdateCommand cashUpdateCommand) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public List<TariffPublishedCommand> publishTariffList() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public TariffReplyCommand processTariffReply(TariffReplyCommand tariffReplyCommand) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public List<CustomerInfoCommand> processCustomerInfo(List<CustomerInfoCommand> customerInfoCommands) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
