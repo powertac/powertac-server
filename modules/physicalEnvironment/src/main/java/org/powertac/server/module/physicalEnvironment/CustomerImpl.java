c0a5239727617d625cfb83d781a6c6abfd547be3
ddauer <ddauer@gmail.com>
2010-12-06 14:44:08 +0100

    Added Physical Environment module

---
+++ modules/physicalEnvironment/src/main/java/org/powertac/server/module/physicalEnvironment/CustomerImpl.java
@@ -0,0 +1,8 @@
+package org.powertac.server.module.physicalEnvironment;
+
+import org.powertac.common.interfaces.PhysicalEnvironment;
+
+class PhysicalEnvironmentImpl implements PhysicalEnvironment {
+
+
+}
