3a7f50850c9aa3669765bd839cfcd8fbf7233d0f
ddauer <ddauer@gmail.com>
2010-12-13 14:20:56 +0100

    Rename WeatherRealDataCommand to WeatherRealData

--- modules/physicalEnvironment/src/main/java/org/powertac/server/module/physicalEnvironment/PhysicalEnvironmentImpl.java
+++ modules/physicalEnvironment/src/main/java/org/powertac/server/module/physicalEnvironment/PhysicalEnvironmentImpl.java
@@ -2,14 +2,14 @@ package org.powertac.server.module.physicalEnvironment;
 
 import org.powertac.common.commands.TimeslotChanged;
 import org.powertac.common.commands.WeatherForecastData;
-import org.powertac.common.commands.WeatherRealDataCommand;
+import org.powertac.common.commands.WeatherRealData;
 import org.powertac.common.interfaces.PhysicalEnvironment;
 
 class PhysicalEnvironmentImpl implements PhysicalEnvironment {
 
 
     @Override
-    public WeatherRealDataCommand generateRealWeatherData(TimeslotChanged timeslotChanged) {
+    public WeatherRealData generateRealWeatherData(TimeslotChanged timeslotChanged) {
         return null;
     }
 
