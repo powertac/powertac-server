e27c77f11e102e4f5d22661855d8c5fec2fbb4b3
Carsten Block <mail@carstenblock.org>
2011-01-05 22:29:18 +0100

    Test stubs added

---
+++ test/unit/org/powertac/taglibs/CompetitionTagLibTests.groovy
@@ -0,0 +1,17 @@
+package org.powertac.taglibs
+
+import grails.test.*
+
+class CompetitionTagLibTests extends TagLibUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
