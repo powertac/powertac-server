bf2f4dfeba502d7ad8e60fd39f3bd78ae78e7768
Carsten Block <mail@carstenblock.org>
2011-01-06 19:43:29 +0100

    Service Interfaces redefined

--- src/java/org/powertac/server/ICommandEncodingService.java
+++ src/java/org/powertac/server/ICommandEncodingService.java
@@ -6,8 +6,8 @@ import org.powertac.common.command.ShoutDoCreateCmd;
  * TODO: Add Description
  *
  * @author Carsten Block
- * @version 1.0, Date: 29.12.10
+ * @version 1.0, Date: 06.01.11
  */
-public interface IStringMessageService {
-  public String sendMessage(String message);
+public interface ICommandEncodingService {
+  public String encodeShoutDoCreateCommand(ShoutDoCreateCmd cmd);
 }
