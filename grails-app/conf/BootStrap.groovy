b1510a9d28dececc732c2208af23aef2ae9251e2
Carsten Block <mail@carstenblock.org>
2011-01-30 17:56:06 +0100

    Domain test instances for competition, broker, product, timeslot now automatically generated in development modus

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,21 @@
+import org.powertac.common.Broker
+import org.powertac.common.Competition
+import org.powertac.common.Product
+import org.powertac.common.Timeslot
+import org.powertac.common.enumerations.ProductType
+
 class BootStrap {
 
-    def init = { servletContext ->
+  def init = { servletContext ->
+    environments {
+      development {
+        def competition = new Competition(name: 'testCompetition', enabled: true, current: true).save()
+        new Broker(competition: competition, userName: 'testUser', apiKey: 'testApiKey-which-needs-to-be-longer-than-32-characters').save()
+        new Product(competition: competition, productType: ProductType.Future).save()
+        new Timeslot(competition: competition, serialNumber: 0).save()
+      }
     }
-    def destroy = {
-    }
+  }
+  def destroy = {
+  }
 }
