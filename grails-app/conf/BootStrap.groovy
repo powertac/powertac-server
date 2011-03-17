6250e2bf95f0a10a6224b7cba2fdac5ac242343e
ddauer <ddauer@gmail.com>
2011-03-17 15:57:18 +0100

    Create default broker

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,18 +3,32 @@ import org.powertac.common.Competition
 import org.powertac.common.Product
 import org.powertac.common.Timeslot
 import org.powertac.common.enumerations.ProductType
+import org.powertac.common.Role
+import org.powertac.common.BrokerRole
 
 class BootStrap {
 
+  def springSecurityService
+
   def init = { servletContext ->
-    environments {
-      development {
-        def competition = new Competition(name: 'testCompetition').save()
-        new Broker(userName: 'testUser', apiKey: 'testApiKey-which-needs-to-be-longer-than-32-characters').save()
-        //new Product(productType: ProductType.Future).save()
-        new Timeslot(serialNumber: 0).save()
-      }
+    // Create admin role
+    def adminRole = Role.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_ADMIN').save(failOnError: true)
+    // Create default broker
+    def adminUser = Broker.findByUsername('defaultBroker') ?: new Broker(
+        username: 'defaultBroker',
+        password: springSecurityService.encodePassword('password'),
+        enabled: true).save(failOnError: true)
+
+    // Add default broker to admin role
+    if (!adminUser.authorities.contains(adminRole)) {
+      BrokerRole.create adminUser, adminRole
     }
+
+//    def competition = new Competition(name: 'testCompetition').save()
+//    new Broker(userName: 'testUser', apiKey: 'testApiKey-which-needs-to-be-longer-than-32-characters').save()
+//    //new Product(productType: ProductType.Future).save()
+//    new Timeslot(serialNumber: 0).save()
+
   }
   def destroy = {
   }
