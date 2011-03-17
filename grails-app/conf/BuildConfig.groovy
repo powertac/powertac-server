78087acecceb1260cd41757b125c805c0873faed
ddauer <ddauer@gmail.com>
2011-03-17 14:22:40 +0100

    Correct build settings

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -7,6 +7,7 @@ grails.project.test.reports.dir = "target/test-reports"
 grails.plugin.location.PowertacCommon = "../powertac-common"
 grails.plugin.location.PowertacServerInterface = "../powertac-server-interface"
 grails.plugin.location.PowertacAccountingService = "../powertac-accounting-service"
+grails.plugin.location.PowertacWebApp = "../powertac-web-app"
 
 grails.project.dependency.resolution = {
   // inherit Grails' default dependencies
