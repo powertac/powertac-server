519f4dd13036bf5885bc43c0b0cad86b568df726
Nguyen Nguyen <nguyen.a.nguyen@gmail.com>
2011-05-11 08:08:38 -0500

    comment out import for SpringSecurity

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,4 +1,4 @@
-import grails.plugins.springsecurity.SecurityConfigType
+// import grails.plugins.springsecurity.SecurityConfigType
 
 // locations to search for config files that get merged into the main config
 // config files can either be Java properties files or ConfigSlurper scripts
