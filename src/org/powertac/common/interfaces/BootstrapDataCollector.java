09bac2167d45432c4152f3bc5e4a3fa089fb234d
John Collins <jcollins@cs.umn.edu>
2011-10-25 22:07:27 -0500

    #389 - bootstrap data collector interface

---
+++ src/org/powertac/common/interfaces/BootstrapDataCollector.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2011 by the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.powertac.common.interfaces;
+
+import java.util.List;
+
+/**
+ * Object that collects bootstrap data for initializing brokers.
+ * @author John Collins
+ */
+public interface BootstrapDataCollector
+{
+  /**
+   * Returns a list of bootstrap messages that can be bundled up and sent
+   * to brokers at the beginning of a game.
+   */
+  public List<Object> collectBootstrapData ();
+}
