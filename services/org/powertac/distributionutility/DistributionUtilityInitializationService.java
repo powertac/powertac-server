efd8f20bdc8cddc70dc60b20ccf053a87cf270bf
John Collins <jcollins@cs.umn.edu>
2011-10-31 18:59:13 -0500

    #371 - bootstrap mode working

--- services/org/powertac/distributionutility/DistributionUtilityInitializationService.java
+++ services/org/powertac/distributionutility/DistributionUtilityInitializationService.java
@@ -8,37 +8,40 @@ import org.powertac.common.PluginConfig;
 import org.powertac.common.interfaces.InitializationService;
 import org.powertac.common.repo.PluginConfigRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-class DistributionUtilityInitializationService implements InitializationService {
-	private static boolean transactional = true;
-	Logger log = Logger.getLogger(this.getClass().getName());
+@Service
+class DistributionUtilityInitializationService
+implements InitializationService
+{
+  Logger log = Logger.getLogger(this.getClass().getName());
 
-	@Autowired
-	private DistributionUtilityService distributionUtilityService;
+  @Autowired
+  private DistributionUtilityService distributionUtilityService;
 
-	@Autowired
-	private PluginConfigRepo pluginConfigRepo;
+  @Autowired
+  private PluginConfigRepo pluginConfigRepo;
 
-	public void setDefaults() {
-		PluginConfig config = new PluginConfig("DistributionUtility", "");
-		// TODO implement configurable values for below variables.
-		// configuration: ['distributionFeeMin': '0.003',
-		// 'distributionFeeMax': '0.03',
-		// 'balancingCostMin': '0.02',
-		// 'balancingCostMax': '0.06',
-		// 'defaultSpotPrice': '50.0']);
-		// config.save();
-	}
+  public void setDefaults() {
+    PluginConfig config = pluginConfigRepo.makePluginConfig("DistributionUtility", "");
+    // TODO implement configurable values for below variables.
+    // configuration: ['distributionFeeMin': '0.003',
+    // 'distributionFeeMax': '0.03',
+    // 'balancingCostMin': '0.02',
+    // 'balancingCostMax': '0.06',
+    // 'defaultSpotPrice': '50.0']);
+    // config.save();
+  }
 
-	public String initialize(Competition competition,
-			List<String> completedInits) {
-		PluginConfig duConfig = pluginConfigRepo.findByRoleName("DistributionUtility");
-		if (duConfig == null) {
-			log.error("PluginConfig for DistributionUtility does not exist");
-			return "fail";
-		} else {
-			distributionUtilityService.init(duConfig);
-			return "DistributionUtility";
-		}
-	}
+  public String initialize(Competition competition,
+                           List<String> completedInits) {
+    PluginConfig duConfig = pluginConfigRepo.findByRoleName("DistributionUtility");
+    if (duConfig == null) {
+      log.error("PluginConfig for DistributionUtility does not exist");
+      return "fail";
+    } else {
+      distributionUtilityService.init(duConfig);
+      return "DistributionUtility";
+    }
+  }
 }
