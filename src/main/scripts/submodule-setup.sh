2a60f2fed23ef60156d43677554ee6ed848bacbc
John Collins <jcollins@cs.umn.edu>
2011-11-14 17:48:10 -0600

    add submodule update script

---
+++ src/main/scripts/submodule-setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# populate and update submodules
+# usage:
+#  submodule-setup.sh git-branch-name
+
+git submodule init
+git submodule update
+
+branch=$1
+tag=path
+while read lhs eq rhs; do
+    if [ $lhs = $tag ]; then
+	pushd rhs
+	git checkout $branch
+	git pull
+    fi
+done < .gitmodules
