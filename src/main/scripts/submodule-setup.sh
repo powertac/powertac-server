3fef458921797bea7ce50ea01749411d3cfc1054
John Collins <jcollins@cs.umn.edu>
2011-11-14 17:57:16 -0600

    script update

--- src/main/scripts/submodule-setup.sh
+++ src/main/scripts/submodule-setup.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 # populate and update submodules
 # usage:
-#  submodule-setup.sh git-branch-name
+#  submodule-setup.sh git-repo-name git-branch-name
+#  example: submodule-setup.sh origin master
 
 git submodule init
 git submodule update
 
-branch=$1
+repo=$1
+branch=$2
 tag=path
 while read lhs eq rhs; do
     if [ $lhs = $tag ]; then
 	#echo $lhs $eq $rhs
 	pushd $rhs
-	git checkout $branch
+	git checkout $repo $branch
 	git pull
 	popd
     fi
