66480f5ebe831adde8441540e765544f3ddd33f1
Nguyen Nguyen <nguyen@entagen.com>
2012-01-11 11:39:29 -0600

    Revert back to use findBySerialNumber instead. Nikolaus discovered npe issue with findOrCreateBySerialNumber.  Will come back to look at this issue later

--- src/main/java/org/powertac/common/xml/TimeslotConverter.java
+++ src/main/java/org/powertac/common/xml/TimeslotConverter.java
@@ -45,7 +45,7 @@ public class TimeslotConverter implements SingleValueConverter
   @Override
   public Object fromString (String serial)
   {
-    return timeslotRepo.findOrCreateBySerialNumber(Integer.valueOf(serial));
+    return timeslotRepo.findBySerialNumber(Integer.valueOf(serial));
   }
 
   @Override
