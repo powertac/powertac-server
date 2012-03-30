6174dd02b8dcfdcaa2eb24d3a3e95c18e77be650
John Collins <jcollins@cs.umn.edu>
2012-03-30 15:37:37 -0500

    list configs

---
+++ src/main/java/org/powertac/common/config/ConfigurationLister.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.powertac.common.config;
+
+import org.apache.log4j.Logger;
+
+/**
+ * This is a helper class for generating a sample configuration file from configured instances.
+ * To use it, you need to create and configure a set of instances, then open an output stream
+ * and run an instance of this class across the configured instances to generate the output
+ * text.
+ * @author John Collins
+ */
+public class ConfigurationLister
+{
+  static private Logger log = Logger.getLogger(ConfigurationLister.class);
+
+}
