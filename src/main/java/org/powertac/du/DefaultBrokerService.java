80d4332c000e9783dac88994ad5a1f93833ff282
John Collins <jcollins@cs.umn.edu>
2012-04-17 17:58:31 -0500

    #489 - add default tariff for interruptible-consumption

--- src/main/java/org/powertac/du/DefaultBrokerService.java
+++ src/main/java/org/powertac/du/DefaultBrokerService.java
@@ -121,6 +121,7 @@ public class DefaultBrokerService
   
   // local state
   private TariffSpecification defaultConsumption;
+  private TariffSpecification defaultInterruptibleConsumption;
   private TariffSpecification defaultProduction;
   private HashMap<TariffSpecification, 
                   HashMap<CustomerInfo, CustomerRecord>> customerSubscriptions;
@@ -187,6 +188,13 @@ public class DefaultBrokerService
     tariffMarketService.setDefaultTariff(defaultConsumption);
     customerSubscriptions.put(defaultConsumption,
                               new HashMap<CustomerInfo, CustomerRecord>());
+
+    defaultInterruptibleConsumption =
+            new TariffSpecification(face, PowerType.INTERRUPTIBLE_CONSUMPTION)
+                .addRate(new Rate().withValue(defaultConsumptionRate));
+    tariffMarketService.setDefaultTariff(defaultInterruptibleConsumption);
+    customerSubscriptions.put(defaultInterruptibleConsumption,
+                          new HashMap<CustomerInfo, CustomerRecord>());
 
     defaultProduction = new TariffSpecification(face, PowerType.PRODUCTION)
         .addRate(new Rate().withValue(defaultProductionRate));
