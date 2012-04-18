df251a968c2bd8b4da55534ad4f2ff80d8b29cdd
John Collins <jcollins@cs.umn.edu>
2012-04-18 12:59:51 -0500

    #522 - add web-accessible control interface

--- src/main/java/org/powertac/common/interfaces/CompetitionSetup.java
+++ src/main/java/org/powertac/common/interfaces/CompetitionSetup.java
@@ -15,6 +15,8 @@
  */
 package org.powertac.common.interfaces;
 
+import java.util.List;
+
 /**
  * Handles the pre-game competition setup process. To start a simulation,
  * one must call preGame(), followed by CompetitionControl.runOnce().
@@ -27,5 +29,46 @@ public interface CompetitionSetup
    * to their default state.
    */
   public void preGame ();
-
+  
+  /**
+   * Starts a bootstrap session with parameters. Result is null if successful,
+   * otherwise contains an error message. Parameters are:
+   * <ul>
+   * <li>bootFilename is the (required)
+   * name of an output file where the bootstrap dataset will be stored.</li>
+   * <li>configFilename is the (optional) name of a server-configuration file;
+   * if the name contains a ":" it will be treated as a URL.</li>
+   * <li>logfileSuffix is an (optional) filename suffix for the log output
+   * files; default value is "boot".</li>
+   * </ul> 
+   */
+  public String bootSession (String bootFilename,
+                             String configFilename,
+                             String logfileSuffix);
+  
+  /**
+   * Starts a simulation session with parameters, in a new thread.
+   * Result is null if successful, otherwise contains an error message.
+   * Parameters are:
+   * <ul>
+   * <li>bootData is the (required) name of an input file containing a
+   * bootstrap dataset for the simulation; it will be treated as a URL if it
+   * contains a ":" character.</li>
+   * <li>config is the (optional) name of a server-configuration file;
+   * if the name contains a ":" it will be treated as a URL.</li>
+   * <li>jmsUrl is the URL on which the server should listen for JMS connections.
+   * This is required if the server is to interact with brokers on separate
+   * machines. If given, all brokers must use this URL to contact the server.
+   * <li>logfileSuffix is an (optional) filename suffix for the log output
+   * files; default value is "sim".</li>
+   * <li>brokerUsernames is a list of Strings giving the usernames of brokers
+   * who are expected to log in to the simulation before it starts. If empty,
+   * the simulation will start without brokers.</li>
+   * </ul> 
+   */
+  public String simSession (String bootData,
+                            String config,
+                            String jmsUrl,
+                            String logfileSuffix,
+                            List<String> brokerUsernames);
 }
