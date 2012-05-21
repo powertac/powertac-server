7ebe722b28b19985a416d128f43a9d54b420abc8
John Collins <jcollins@cs.umn.edu>
2012-05-21 14:25:03 -0500

    set up for remote visualizers

--- src/main/java/org/powertac/server/VisualizerProxyService.java
+++ src/main/java/org/powertac/server/VisualizerProxyService.java
@@ -1,27 +1,98 @@
 package org.powertac.server;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.powertac.common.Competition;
+import org.powertac.common.XMLMessageConverter;
+import org.powertac.common.config.ConfigurableValue;
+import org.powertac.common.interfaces.InitializationService;
+import org.powertac.common.interfaces.ServerConfiguration;
 import org.powertac.common.interfaces.VisualizerMessageListener;
 import org.powertac.common.interfaces.VisualizerProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.stereotype.Service;
 
+/**
+ * Connects the server with a Visualizer
+ * @author John Collins
+ */
 @Service
-public class VisualizerProxyService implements VisualizerProxy
+public class VisualizerProxyService
+implements VisualizerProxy, InitializationService
 {
+  @Autowired
+  private ServerConfiguration serverConfig;
+  
+  @Autowired
+  private JmsManagementService jmsManagementService;
+
+  @Autowired
+  private JmsTemplate template;
+
+  @Autowired
+  private XMLMessageConverter converter;
 
   private ArrayList<VisualizerMessageListener> listeners =
       new ArrayList<VisualizerMessageListener>();
   
+  @ConfigurableValue(valueType = "Boolean",
+          description = "true to operate with remote visualizer")
+  private boolean remoteVisualizer = false;
+  
+  @ConfigurableValue(valueType = "String",
+          description = "name of queue for remote visualizer")
+  private String visualizerQueueName = "visualizer";
+  
+  @Override
   public void registerVisualizerMessageListener (VisualizerMessageListener listener)
   {
     if (!listeners.contains(listener))
       listeners.add(listener);
   }
 
+  @Override
   public void forwardMessage (Object message)
   {
     for (VisualizerMessageListener listener : listeners)
       listener.receiveMessage(message);
+    if (remoteVisualizer) {
+      // send messages to queue
+      final String text = converter.toXML(message);
+
+      template.send(visualizerQueueName, new MessageCreator() {
+        @Override
+        public Message createMessage (Session session) throws JMSException
+        {
+          TextMessage message = session.createTextMessage(text);
+          return message;
+        }
+      });
+    }
+  }
+
+  @Override
+  public void setDefaults ()
+  {
+    // stub
+  }
+
+  @Override
+  public String
+    initialize (Competition competition, List<String> completedInits)
+  {
+    serverConfig.configureMe(this);
+    if (remoteVisualizer) {
+      // set up the output queue
+      jmsManagementService.createQueue(visualizerQueueName);
+    }
+    return "VisualizerProxy";
   }
 }
