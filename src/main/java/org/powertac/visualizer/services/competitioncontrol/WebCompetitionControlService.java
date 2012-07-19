182d85faa9a6942541b12527caa50bb87570d48e
John Collins <jcollins@cs.umn.edu>
2012-07-18 20:06:17 -0500

    reformat modules to project standards; correct autowire setup for CustomersBean

--- src/main/java/org/powertac/visualizer/services/competitioncontrol/WebCompetitionControlService.java
+++ src/main/java/org/powertac/visualizer/services/competitioncontrol/WebCompetitionControlService.java
@@ -27,102 +27,126 @@ import org.springframework.stereotype.Service;
  * 
  */
 @Service
-public class WebCompetitionControlService {
+public class WebCompetitionControlService
+{
 
-	static private Logger log = Logger.getLogger(WebCompetitionControlService.class);
+  static private Logger log = Logger
+          .getLogger(WebCompetitionControlService.class);
 
-	@Autowired
-	private VisualizerResourceLoaderService resourceLoader;
+  @Autowired
+  private VisualizerResourceLoaderService resourceLoader;
 
-	@Autowired
-	private CompetitionSetup css;
+  @Autowired
+  private CompetitionSetup css;
 
-	@Autowired
-	private CompetitionControl competitionControl;
+  @Autowired
+  private CompetitionControl competitionControl;
 
-	@Autowired
-	private VisualizerService visualizerService;
+  @Autowired
+  private VisualizerService visualizerService;
 
-	@Autowired
-	private GameParamatersBean gameParamaters;
+  @Autowired
+  private GameParamatersBean gameParamaters;
 
-	@Autowired
-	private VisualizerProxy visualizerProxy;
+  @Autowired
+  private VisualizerProxy visualizerProxy;
 
-	private String message;
-	
+  private String message;
 
-	public void runSim() {
+  public void runSim ()
+  {
 
-		if (!competitionControl.isRunning()) {
+    if (!competitionControl.isRunning()) {
 
-			visualizerService.init(visualizerProxy);
+      visualizerService.init(visualizerProxy);
 
-			List<String> names = new ArrayList<String>();
-			for (Iterator iterator = gameParamaters.getBrokers().iterator(); iterator.hasNext();) {
-				FakeBroker type = (FakeBroker) iterator.next();
-				names.add(type.getName());
-			}
-			// web components treat empty forms as "", not null.
-			String boot = gameParamaters.getBootstrapData().equals("") ? null : gameParamaters.getBootstrapData();
-			String serverConfig = gameParamaters.getServerConfig().equals("") ? null : gameParamaters.getServerConfig();
-			String jmsUrl = gameParamaters.getJmsUrl().equals("") ? null : gameParamaters.getJmsUrl();
-			String logSuffix = gameParamaters.getLogSuffix().equals("") ? null : gameParamaters.getLogSuffix();
+      List<String> names = new ArrayList<String>();
+      for (Iterator iterator = gameParamaters.getBrokers().iterator(); iterator
+              .hasNext();) {
+        FakeBroker type = (FakeBroker) iterator.next();
+        names.add(type.getName());
+      }
+      // web components treat empty forms as "", not null.
+      String boot =
+        gameParamaters.getBootstrapData().equals("")? null: gameParamaters
+                .getBootstrapData();
+      String serverConfig =
+        gameParamaters.getServerConfig().equals("")? null: gameParamaters
+                .getServerConfig();
+      String jmsUrl =
+        gameParamaters.getJmsUrl().equals("")? null: gameParamaters.getJmsUrl();
+      String logSuffix =
+        gameParamaters.getLogSuffix().equals("")? null: gameParamaters
+                .getLogSuffix();
 
-			System.out.println(boot + serverConfig + jmsUrl + logSuffix + names);
+      System.out.println(boot + serverConfig + jmsUrl + logSuffix + names);
 
-			String result = css.simSession(boot, serverConfig, jmsUrl, logSuffix, names);
+      String result =
+        css.simSession(boot, serverConfig, jmsUrl, logSuffix, names, null);
 
-			if (result == null) {
-				message = "Simulation started.";
-			} else {
-				message = "ERROR: " + result;
-			}
+      if (result == null) {
+        message = "Simulation started.";
+      }
+      else {
+        message = "ERROR: " + result;
+      }
 
-		} else {
-			message = "Unable to run a sim game. The competition is already in progress.";
-		}
-	}
+    }
+    else {
+      message =
+        "Unable to run a sim game. The competition is already in progress.";
+    }
+  }
 
-	public void runBoot() {
+  public void runBoot ()
+  {
 
-		if (!competitionControl.isRunning()) {
+    if (!competitionControl.isRunning()) {
 
-			visualizerService.init(visualizerProxy);
+      visualizerService.init(visualizerProxy);
 
-			// web components treat empty forms as "", not null.
-			String bootFilename = gameParamaters.getBootstrapFilename().equals("") ? null : gameParamaters.getBootstrapFilename();
-			String serverConfig = gameParamaters.getServerConfig().equals("") ? null : gameParamaters.getServerConfig();
-			String logSuffix = gameParamaters.getLogSuffix().equals("") ? null : gameParamaters.getLogSuffix();
+      // web components treat empty forms as "", not null.
+      String bootFilename =
+        gameParamaters.getBootstrapFilename().equals("")? null: gameParamaters
+                .getBootstrapFilename();
+      String serverConfig =
+        gameParamaters.getServerConfig().equals("")? null: gameParamaters
+                .getServerConfig();
+      String logSuffix =
+        gameParamaters.getLogSuffix().equals("")? null: gameParamaters
+                .getLogSuffix();
 
-			System.out.println(bootFilename + serverConfig + logSuffix);
+      System.out.println(bootFilename + serverConfig + logSuffix);
 
-			String result = css.bootSession(bootFilename, serverConfig, logSuffix);
+      String result = css.bootSession(bootFilename, serverConfig, logSuffix);
 
-			if (result == null) {
+      if (result == null) {
 
-				message = "Bootstrap mode started.";
-			} else {
-				message = "ERROR: " + result;
-			}
-		} else {
-			message = "Unable to run a bootstrap game. The competition is already in progress.";
-		}
-	}
+        message = "Bootstrap mode started.";
+      }
+      else {
+        message = "ERROR: " + result;
+      }
+    }
+    else {
+      message =
+        "Unable to run a bootstrap game. The competition is already in progress.";
+    }
+  }
 
-	public void shutDown() {
-		if (competitionControl.isRunning()) {
-			competitionControl.shutDown();
-			message = "Shut down is complete.";
-		} else {
-			message = "There is no running game to shut down.";
-		}
-	}
+  public void shutDown ()
+  {
+    if (competitionControl.isRunning()) {
+      competitionControl.shutDown();
+      message = "Shut down is complete.";
+    }
+    else {
+      message = "There is no running game to shut down.";
+    }
+  }
 
-	public String getMessage() {
-		return message;
-	}
-
-
-
+  public String getMessage ()
+  {
+    return message;
+  }
 }
