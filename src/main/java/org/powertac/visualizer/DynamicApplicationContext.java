ab51f12133ea2388adccb9e53d6731e9101f9fa3
John Collins <jcollins@cs.umn.edu>
2012-12-16 11:51:06 -0600

    #644 - deploy war without dependencies

--- src/main/java/org/powertac/visualizer/DynamicApplicationContext.java
+++ src/main/java/org/powertac/visualizer/DynamicApplicationContext.java
@@ -7,8 +7,9 @@ public class DynamicApplicationContext extends XmlWebApplicationContext {
 
   protected String[] getDefaultConfigLocations () {
     String cmd = System.getProperty("sun.java.command");
+    System.out.println("Command: " + cmd);
 
-    if (cmd.contains("jetty")) {
+    if (cmd.contains("jetty") || cmd.contains("web")) {
       return new String[] {
           DEFAULT_CONFIG_LOCATION_PREFIX + "spring/visualizer.embedded.xml"};
     } else {
