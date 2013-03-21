a8e2659a16153be6e53419182c98e0ac7de9aa64
John Collins <jcollins@cs.umn.edu>
2013-03-21 11:09:13 -0500

    #667 - fix seed ID values to allow repeats

--- src/main/java/org/powertac/common/RandomSeed.java
+++ src/main/java/org/powertac/common/RandomSeed.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2011 the original author or authors.
+ * Copyright 2009-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,6 +26,8 @@ import org.powertac.common.state.StateChange;
  * RandomSeed directly. Instead, please request seeds through 
  * RandomSeedRepo.getRandomSeed(). This way, your code will work the same
  * whether using new seeds or replaying a previous simulation.</p>
+ * <p>State log entry format:<br/>
+ * <code>requesterClass::requesterId::purpose::value</code></p>
  *
  * @author Carsten Block, John Collins
  * @version 1.0 - January 01, 2011
