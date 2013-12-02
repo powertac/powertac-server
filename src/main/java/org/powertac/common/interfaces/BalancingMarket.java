0da856d9c6bf8d30bd7719d8db94c01012908d78
John Collins <jcollins@cs.umn.edu>
2013-12-01 20:58:16 -0600

    #578 - new interface BalancingMarket

---
+++ src/main/java/org/powertac/common/interfaces/BalancingMarket.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an
+ * "AS IS" BASIS,  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.powertac.common.interfaces;
+
+import org.powertac.common.Broker;
+
+/**
+ * Interface that defines the publicly accessible methods
+ * a Power TAC balancing market has to implement.
+ *
+ * @author John Collins
+ */
+public interface BalancingMarket
+{
+  /**
+   * Returns the market balance for a given broker.
+   */
+  public double getMarketBalance (Broker broker);
+
+  /**
+   * Returns the net regulation energy (positive for up-regulation,
+   * negative for down-regulation) for a given broker.
+   */
+  public double getRegulation (Broker broker);
+}
