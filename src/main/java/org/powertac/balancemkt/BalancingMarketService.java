1fec3d30b277ef848c795c66b7437796d583b8df
John Collins <jcollins@cs.umn.edu>
2013-12-01 20:52:37 -0600

    initial refactoring

---
+++ src/main/java/org/powertac/balancemkt/BalancingMarketService.java
@@ -0,0 +1,365 @@
+/*
+ * Copyright 2009-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an
+ * "AS IS" BASIS,  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package org.powertac.balancemkt;
+
+import java.lang.reflect.Constructor;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.joda.time.Instant;
+import org.powertac.common.config.ConfigurableValue;
+import org.powertac.common.interfaces.Accounting;
+import org.powertac.common.interfaces.BalancingMarket;
+import org.powertac.common.interfaces.CapacityControl;
+import org.powertac.common.interfaces.InitializationService;
+import org.powertac.common.interfaces.ServerConfiguration;
+import org.powertac.common.Broker;
+import org.powertac.common.Competition;
+import org.powertac.common.Orderbook;
+import org.powertac.common.RandomSeed;
+import org.powertac.common.Timeslot;
+import org.powertac.common.interfaces.TimeslotPhaseProcessor;
+import org.powertac.common.msg.BalancingOrder;
+import org.powertac.common.repo.BrokerRepo;
+import org.powertac.common.repo.OrderbookRepo;
+import org.powertac.common.repo.RandomSeedRepo;
+import org.powertac.common.repo.TariffRepo;
+import org.powertac.common.repo.TimeslotRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BalancingMarketService
+extends TimeslotPhaseProcessor
+implements BalancingMarket, SettlementContext, InitializationService
+{
+  Logger log = Logger.getLogger(this.getClass().getSimpleName());
+
+  @Autowired
+  private BrokerRepo brokerRepo;
+
+  @Autowired
+  private TimeslotRepo timeslotRepo;
+
+  @Autowired
+  private OrderbookRepo orderbookRepo;
+  
+  @Autowired
+  private TariffRepo tariffRepo;
+
+  @Autowired
+  private Accounting accountingService;
+  
+  @Autowired
+  private CapacityControl capacityControlService;
+  
+  @Autowired
+  private ServerConfiguration serverProps;
+
+  @Autowired
+  private RandomSeedRepo randomSeedService;
+  private RandomSeed randomGen;
+
+  @ConfigurableValue(valueType = "Double",
+      description = "Low end of balancing cost range for simple settlement processor")
+  private double balancingCostMin = -0.01;
+
+  @ConfigurableValue(valueType = "Double",
+      description = "High end of balancing cost range for simple settlement processor")
+  private double balancingCostMax = -0.02;
+
+  @ConfigurableValue(valueType = "Double",
+      publish = true,
+      description = "Balancing cost for simple settlement processor: overrides random value selection")
+  private Double balancingCost = null;
+  
+  @ConfigurableValue(valueType = "Double",
+          publish = true,
+          description = "Slope of up-regulation cost /kwh")
+  private double pPlusPrime = 0.0; // .00002/kwh
+  
+  @ConfigurableValue(valueType = "Double",
+          publish = true,
+          description = "Slope of down-regulation cost /kwh")
+  private double pMinusPrime = 0.0; // -.00002/kwh
+
+  @ConfigurableValue(valueType = "Double",
+      publish = true,
+      description = "Spot price/mwh used if unavailable from wholesale market")
+  private double defaultSpotPrice = 30.0; // per mwh
+  
+  @ConfigurableValue(valueType = "String",
+          publish = true,
+          description = "Balancing settlement processing: blank for no controllable capacity, "
+          + "\"static\" for per-timeslot processing of balancing orders")
+  private String settlementProcess = "";
+  
+  // map settlement process to strategy instances
+  @SuppressWarnings("serial")
+  private Map<String, Class<?>> settlementMap =
+          new HashMap<String, Class<?>> () {{
+            put("simple", SimpleSettlementProcessor.class);
+            put("static", StaticSettlementProcessor.class);
+            put("dynamic", DynamicSettlementProcessor.class);
+          }};
+
+  private Map<Broker, ChargeInfo> balancingResults = null;
+
+  @Override
+  public void setDefaults ()
+  {
+  }
+
+  /**
+   * Computes actual distribution and balancing costs by random selection
+   */
+  @Override
+  public String initialize (Competition competition, List<String> completedInits)
+  {
+    super.init();
+    balancingCost = null;
+
+    serverProps.configureMe(this);
+    
+    // compute randomly-generated values if not overridden
+    randomGen = randomSeedService.getRandomSeed("BalancingMarketService",
+                                                0, "model");
+    if (null == balancingCost)
+      balancingCost = (balancingCostMin + randomGen.nextDouble()
+                       * (balancingCostMax - balancingCostMin));
+    log.info("Configured BM: balancing cost = " + balancingCost
+             + ", (pPlus',pMinus') = (" + pPlusPrime + "," + pMinusPrime + ")");
+    
+    serverProps.publishConfiguration(this);
+    return "DistributionUtility";
+  }
+
+  @Override
+  public void activate (Instant time, int phaseNumber)
+  {
+    log.info("Activate");
+    List<Broker> brokerList = brokerRepo.findRetailBrokers();
+    if (brokerList == null) {
+      log.error("Failed to retrieve retail broker list");
+      return;
+    }
+
+    // Run the balancing market
+    // Transactions are posted to the Accounting Service and Brokers are
+    // notified of balancing transactions
+    balancingResults =
+            balanceTimeslot(timeslotRepo.currentTimeslot(), brokerList);
+  }
+
+  /**
+   * Generates a list of Transactions that balance the overall market.
+   * Transactions are generated on a per-broker basis depending on the broker's
+   * balance within its own market.
+   * 
+   * @return List of ChargeInfo instances
+   */
+  public HashMap<Broker, ChargeInfo>
+  balanceTimeslot (Timeslot currentTimeslot, List<Broker> brokerList)
+  {
+    HashMap<Broker, ChargeInfo> chargeInfoMap = new HashMap<Broker, ChargeInfo>();
+
+    // create the ChargeInfo instances for each broker
+    for (Broker broker : brokerList) {
+      ChargeInfo info = new ChargeInfo(broker, getMarketBalance(broker));
+      chargeInfoMap.put(broker, info);
+    }
+    
+    // retrieve and allocate the balancing orders
+    Collection<BalancingOrder> boc = tariffRepo.getBalancingOrders();
+    for (BalancingOrder order : boc) {
+      ChargeInfo info = chargeInfoMap.get(order.getBroker());
+      info.addBalancingOrder(order);
+    }
+
+    // gather up the list of ChargeInfo instances and settle
+    log.info("balancing prices: pPlus=" + getPPlus()
+             + ", pMinus=" + getPMinus());
+    List<ChargeInfo> brokerData = new ArrayList<ChargeInfo>(chargeInfoMap.values());
+    getSettlementProcessor().settle(this, brokerData);
+    
+    // add balancing transactions - note that debits/credits for balancing
+    // orders (p2 values) will already have been posted in the process of
+    // exercising orders.
+    for (ChargeInfo info : brokerData) {
+      double balanceCharge = info.getBalanceChargeP1();
+      if (balanceCharge != 0.0) {
+        accountingService.addBalancingTransaction(info.getBroker(),
+                                                  info.getNetLoadKWh(),
+                                                  balanceCharge);
+      }
+    }
+    return chargeInfoMap;
+  }
+
+  /**
+   * Returns the difference between a broker's current market position and its
+   * net load. Note: market position is computed in MWh and net load is computed
+   * in kWh, conversion is needed to compute the difference.
+   * 
+   * @return a broker's current energy balance within its market. Pos for
+   *         over-production, neg for under-production
+   */
+  @Override
+  public double getMarketBalance (Broker broker)
+  {
+    double result = accountingService.getCurrentMarketPosition(broker) * 1000.0
+                    + accountingService.getCurrentNetLoad(broker);
+    log.info("market balance for " + broker.getUsername() + ": " + result);
+    return result;
+  }
+
+  /**
+   * Returns the net balancing result for a given broker. Valid only after
+   * service activation within a given timeslot.
+   */
+  @Override
+  public double getRegulation (Broker broker)
+  {
+    return balancingResults.get(broker).getCurtailment();
+  }
+
+  /**
+   * Returns the spot market price - the clearing price for the current timeslot
+   * in the most recent trading period.
+   */
+  double getSpotPrice ()
+  {
+    Double result = defaultSpotPrice;
+    // most recent trade is determined by Competition parameters
+    // orderbooks have timeslot and execution time
+    Orderbook ob =
+        orderbookRepo.findSpotByTimeslot(timeslotRepo.currentTimeslot());
+    if (ob != null) {
+      result = ob.getClearingPrice();
+    }
+    else {
+      log.info("null Orderbook");
+    }
+    return result / 1000.0; // convert to kwh
+  }
+  
+  /**
+   * Returns the maximum price for energy in the current timeslot
+   */
+  @Override
+  public double getPPlus ()
+  {
+    double result = defaultSpotPrice;
+    List<Orderbook> obs = 
+        orderbookRepo.findAllByTimeslot(timeslotRepo.currentTimeslot());
+    if (obs != null && obs.size() > 0) {
+      Double max = null;
+      for (Orderbook ob : obs) {
+        Double price = ob.getClearingPrice();
+        if (price != null && (max == null || price > max))
+          max = price;
+      }
+      if (max != null)
+        result = max;
+    }
+    return result / 1000.0;
+  }
+  
+  /**
+   * Returns the minimum price for energy in the current timeslot
+   */
+  @Override
+  public double getPMinus ()
+  {
+    double result = defaultSpotPrice;
+    List<Orderbook> obs = 
+        orderbookRepo.findAllByTimeslot(timeslotRepo.currentTimeslot());
+    if (obs != null && obs.size() > 0) {
+      Double min = null;
+      for (Orderbook ob : obs) {
+        Double price = ob.getClearingPrice();
+        if (price != null && (min == null || price < min))
+          min = price;
+      }
+      if (min != null)
+        result = min;
+    }
+    return result / 1000.0;
+  }
+
+  @Override
+  public double getPPlusPrime ()
+  {
+    return pPlusPrime;
+  }
+
+  @Override
+  public double getPMinusPrime ()
+  {
+    return pMinusPrime;
+  }
+
+  // ---------- Getters and setters for settlement processsors ---------
+
+  double getBalancingCostMin ()
+  {
+    return balancingCostMin;
+  }
+
+  double getBalancingCostMax ()
+  {
+    return balancingCostMax;
+  }
+
+  @Override
+  public Double getBalancingCost ()
+  {
+    return balancingCost;
+  }
+
+  double getDefaultSpotPrice ()
+  {
+    return defaultSpotPrice;
+  }
+
+  private SettlementProcessor getSettlementProcessor ()
+  {
+    // determine and record settlement process
+    if (settlementProcess.equals(""))
+      settlementProcess = "simple";
+    Class<?> processor = settlementMap.get(settlementProcess);
+    if (null == processor) {
+      log.error("Null settlement processor for " + settlementProcess);
+      processor = settlementMap.get("simple");
+    }
+    SettlementProcessor result = null;
+    try {
+      Constructor<?> constructor =
+              processor.getDeclaredConstructor(TariffRepo.class,
+                                               CapacityControl.class);
+      result = (SettlementProcessor) constructor.newInstance(tariffRepo, capacityControlService);
+    }
+    catch (Exception e) {
+      log.error("cannot create settlement processor: " + e.toString());
+    }
+    return result;
+  }
+}
---
+++ src/test/java/org/powertac/balancemkt/BalancingMarketServiceTests.java
@@ -0,0 +1,373 @@
+package org.powertac.balancemkt;
+
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+import static org.powertac.util.ListTools.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.MapConfiguration;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.Instant;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.powertac.balancemkt.BalancingMarketService;
+import org.powertac.common.config.Configurator;
+import org.powertac.common.enumerations.PowerType;
+import org.powertac.common.interfaces.Accounting;
+import org.powertac.common.interfaces.ServerConfiguration;
+import org.powertac.common.msg.BalancingOrder;
+import org.powertac.common.Broker;
+import org.powertac.common.Competition;
+import org.powertac.common.Rate;
+import org.powertac.common.Tariff;
+import org.powertac.common.TariffSpecification;
+import org.powertac.common.TimeService;
+import org.powertac.common.Timeslot;
+import org.powertac.common.repo.BrokerRepo;
+import org.powertac.common.repo.OrderbookRepo;
+import org.powertac.common.repo.TariffRepo;
+import org.powertac.common.repo.TimeslotRepo;
+import org.powertac.util.Predicate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:test-config.xml" })
+public class BalancingMarketServiceTests
+{
+
+  @Autowired
+  private TimeService timeService;
+
+  @Autowired
+  private TimeslotRepo timeslotRepo;
+
+  @Autowired
+  private BrokerRepo brokerRepo;
+
+  @Autowired
+  private TariffRepo tariffRepo;
+
+  @Autowired
+  private OrderbookRepo orderbookRepo;
+  
+  @Autowired
+  private Accounting accountingService;
+  
+  @Autowired
+  private ServerConfiguration serverPropertiesService;
+
+  @Autowired
+  private BalancingMarketService balancingMarketService;
+
+  private Competition comp;
+  private Configurator config;
+  private List<Broker> brokerList = new ArrayList<Broker>();
+  private List<TariffSpecification> tariffSpecList = new ArrayList<TariffSpecification>();
+  private List<Tariff> tariffList = new ArrayList<Tariff>();
+  private DateTime start;
+
+  @Before
+  public void setUp ()
+  {
+    // create a Competition, needed for initialization
+    comp = Competition.newInstance("du-test");
+    Competition.setCurrent(comp);
+    
+    Instant base =
+            Competition.currentCompetition().getSimulationBaseTime().plus(TimeService.DAY);
+    start = new DateTime(start, DateTimeZone.UTC);
+    timeService.setCurrentTime(base);
+    timeslotRepo.makeTimeslot(base);
+    //timeslotRepo.currentTimeslot().disable();// enabled: false);
+    reset(accountingService);
+
+    // Create 3 test brokers
+    Broker broker1 = new Broker("testBroker1");
+    brokerRepo.add(broker1);
+    brokerList.add(broker1);
+
+    Broker broker2 = new Broker("testBroker2");
+    brokerRepo.add(broker2);
+    brokerList.add(broker2);
+
+    Broker broker3 = new Broker("testBroker3");
+    brokerRepo.add(broker3);
+    brokerList.add(broker3);
+
+    // Set up serverProperties mock
+    config = new Configurator();
+    doAnswer(new Answer<Object>() {
+      @Override
+      public Object answer(InvocationOnMock invocation) {
+        Object[] args = invocation.getArguments();
+        config.configureSingleton(args[0]);
+        return null;
+      }
+    }).when(serverPropertiesService).configureMe(anyObject());
+  }
+
+  @After
+  public void tearDown ()
+  {
+    // clear all repos
+    timeslotRepo.recycle();
+    brokerRepo.recycle();
+    tariffRepo.recycle();
+    orderbookRepo.recycle();
+
+    // clear member lists
+    brokerList.clear();
+    tariffSpecList.clear();
+    tariffList.clear();
+  }
+
+  private void initializeService ()
+  {
+    balancingMarketService.setDefaults();
+    TreeMap<String, String> map = new TreeMap<String, String>();
+    map.put("balancemkt.balancingMarketService.balancingCostMin", "-0.06");
+    map.put("balancemkt.balancingMarketService.balancingCostMax", "-0.06");
+    Configuration mapConfig = new MapConfiguration(map);
+    config.setConfiguration(mapConfig);
+    balancingMarketService.initialize(comp, new ArrayList<String>());
+    assertEquals("correct setting", -0.06,
+                 balancingMarketService.getBalancingCostMin(), 1e-6);
+    assertEquals("correct setting", -0.06,
+                 balancingMarketService.getBalancingCostMax(), 1e-6);
+    assertEquals("correct setting", -0.06,
+                 balancingMarketService.getBalancingCost(), 1e-6);
+  }
+
+  @Test
+  public void testGetMarketBalance ()
+  {
+    initializeService();
+    when(accountingService.getCurrentMarketPosition((Broker) anyObject())).thenReturn(1.0);
+    when(accountingService.getCurrentNetLoad((Broker) anyObject())).thenReturn(-500.0);    
+    assertEquals("correct balance",
+                 500.0,
+                 balancingMarketService.getMarketBalance(brokerList.get(0)),
+                 1e-6);
+    when(accountingService.getCurrentNetLoad((Broker) anyObject())).thenReturn(-1000.0);    
+    assertEquals("correct balance",
+                 0.0,
+                 balancingMarketService.getMarketBalance(brokerList.get(0)),
+                 1e-6);
+    when(accountingService.getCurrentNetLoad((Broker) anyObject())).thenReturn(-1500.0);    
+    assertEquals("correct balance",
+                 -500.0,
+                 balancingMarketService.getMarketBalance(brokerList.get(0)),
+                 1e-6);
+  }
+
+  @Test
+  public void testNegImbalancedMarket ()
+  {
+    initializeService();
+    when(accountingService.getCurrentMarketPosition((Broker) anyObject())).thenReturn(0.0);
+    when(accountingService.getCurrentNetLoad((Broker) anyObject())).thenReturn(-50.0);    
+    double marketBalance = -150.0; // Compute market balance
+    Map<Broker, ChargeInfo> theChargeInfoList =
+        balancingMarketService.balanceTimeslot(timeslotRepo.currentTimeslot(),
+                                                   brokerList);
+
+    assertEquals("correct number of balance tx", 3, theChargeInfoList.size());
+    for (ChargeInfo ci : theChargeInfoList.values()) {
+      marketBalance -= ci.getNetLoadKWh();
+    }
+    assertEquals("correct balancing transactions", 0.0, marketBalance, 1e-6);
+  }
+
+  @Test
+  public void TestPosImbalancedMarket ()
+  {
+    initializeService();
+    when(accountingService.getCurrentMarketPosition((Broker) anyObject())).thenReturn(0.0);
+    when(accountingService.getCurrentNetLoad((Broker) anyObject())).thenReturn(50.0);    
+    double marketBalance = 150.0; // Compute market balance
+
+    Map<Broker, ChargeInfo> theChargeInfoList = balancingMarketService.balanceTimeslot(timeslotRepo.currentTimeslot(),
+                                                                                    brokerList);
+
+    assertEquals("correct number of balance tx", 3, theChargeInfoList.size());
+    for (ChargeInfo ci : theChargeInfoList.values()) {
+      marketBalance -= ci.getNetLoadKWh();
+    }
+    assertEquals("correct balancing transactions", 0.0, marketBalance, 1e-6);
+  }
+
+  @Test
+  public void testIndividualBrokerBalancing ()
+  {
+    initializeService();
+    double balance = 0.0;
+
+    when(accountingService.getCurrentMarketPosition((Broker) anyObject())).thenReturn(0.0);
+    when(accountingService.getCurrentNetLoad(brokerList.get(0))).thenReturn(-19599990.0);    
+    when(accountingService.getCurrentNetLoad(brokerList.get(1))).thenReturn(0.0);
+    when(accountingService.getCurrentNetLoad(brokerList.get(2))).thenReturn(8791119.0);    
+
+    // Compute market balance
+    for (Broker b : brokerList) {
+      balance += balancingMarketService.getMarketBalance(b);
+    }
+
+    Map<Broker, ChargeInfo> chargeInfos = balancingMarketService.balanceTimeslot(timeslotRepo.currentTimeslot(),
+                                                                                    brokerList);
+
+    // ensure each broker was balanced correctly
+    for (Broker broker : brokerList) {
+      ChargeInfo ci = chargeInfos.get(broker);
+      assertNotNull("found ChargeInfo", ci);
+      assertEquals("broker correctly balanced",
+                   0.0,
+                   (balancingMarketService.getMarketBalance(broker)
+                           - ci.getNetLoadKWh()),
+                   1e-6);
+      balance -= ci.getNetLoadKWh();
+    }
+    assertEquals("market fully balanced", 0.0, balance, 1e-6);
+  }
+
+  @Test
+  public void testScenario1BalancingCharges ()
+  {
+    initializeService();
+
+    when(accountingService.getCurrentMarketPosition((Broker) anyObject())).thenReturn(0.0);
+    when(accountingService.getCurrentNetLoad(brokerList.get(0))).thenReturn(200.0);    
+    when(accountingService.getCurrentNetLoad(brokerList.get(1))).thenReturn(-400.0);
+    when(accountingService.getCurrentNetLoad(brokerList.get(2))).thenReturn(0.0);    
+
+    // List solution =
+    // balancingMarketService.computeNonControllableBalancingCharges(brokerList)
+    Map<Broker, ChargeInfo> chargeInfos =
+        balancingMarketService.balanceTimeslot(timeslotRepo.currentTimeslot(),
+                                                   brokerList);
+
+    // Correct solution list is [-4, 14, 2] (but negated)
+    ChargeInfo ci = chargeInfos.get(brokerList.get(0)); // BalancingTransaction.findByBroker(brokerList.get(0));
+    assertNotNull("non-null btx, broker 1", ci);
+    assertEquals("correct balancing charge broker1",
+                 4.0, ci.getBalanceCharge(), 1e-6);
+    ci = chargeInfos.get(brokerList.get(1)); // BalancingTransaction.findByBroker(brokerList.get(1));
+    assertNotNull("non-null btx, broker 2", ci);
+    assertEquals("correct balancing charge broker2",
+                 -14.0, ci.getBalanceCharge(), 1e-6);
+    ci = chargeInfos.get(brokerList.get(2)); // BalancingTransaction.findByBroker(brokerList.get(2));
+    assertNotNull("non-null btx, broker 3", ci);
+    assertEquals("correct balancing charge broker3",
+                 -2.0, ci.getBalanceCharge(), 1e-6);
+  }
+
+  @Test
+  public void testSpotPrice ()
+  {
+    initializeService();
+    updatePrices();
+
+    // make sure we can retrieve current spot price
+    assertEquals("correct spot price", 0.0201,
+                 balancingMarketService.getSpotPrice(), 1e-6);
+    assertEquals("correct pMinus", 0.0198,
+                 balancingMarketService.getPMinus(), 1e-6);
+    assertEquals("correct pPlus", 0.0212,
+                 balancingMarketService.getPPlus(), 1e-6);
+  }
+
+  private void updatePrices ()
+  {
+    // add some new timeslots
+    Timeslot ts0 = timeslotRepo.currentTimeslot();
+    long start = timeService.getCurrentTime().getMillis();
+    Timeslot ts1 = timeslotRepo.findByInstant(new Instant(start - TimeService.HOUR * 3));
+    Timeslot ts2 = timeslotRepo.findByInstant(new Instant(start - TimeService.HOUR * 2));
+    Timeslot ts3 = timeslotRepo.findByInstant(new Instant(start - TimeService.HOUR));
+
+    // add some orderbooks
+    orderbookRepo.makeOrderbook(ts3, 33.0);
+    orderbookRepo.makeOrderbook(ts3, 32.0);
+    orderbookRepo.makeOrderbook(ts0, 20.2);
+    orderbookRepo.makeOrderbook(ts0, 21.2);
+    orderbookRepo.makeOrderbook(ts0, 19.8);
+    // this should be the spot price
+    orderbookRepo.makeOrderbook(ts0, 20.1);
+  }
+  
+  // make sure balancing orders are correctly allocated
+  @Test
+  public void testBalancingOrderAllocation ()
+  {
+    initializeService();
+    final Broker b1 = brokerRepo.findByUsername("testBroker1");
+    TariffSpecification b1ts1 =
+            new TariffSpecification(b1, PowerType.INTERRUPTIBLE_CONSUMPTION);
+    b1ts1.addRate(new Rate().withValue(0.12).withMaxCurtailment(0.3));
+    tariffRepo.addSpecification(b1ts1);
+    TariffSpecification b1ts2 =
+            new TariffSpecification(b1, PowerType.INTERRUPTIBLE_CONSUMPTION);
+    b1ts1.addRate(new Rate().withValue(0.10).withMaxCurtailment(0.5));
+    tariffRepo.addSpecification(b1ts2);
+
+    final Broker b2 = brokerRepo.findByUsername("testBroker2");
+    TariffSpecification b2ts1 =
+            new TariffSpecification(b2, PowerType.INTERRUPTIBLE_CONSUMPTION);
+    b1ts1.addRate(new Rate().withValue(0.13).withMaxCurtailment(0.2));
+    tariffRepo.addSpecification(b2ts1);
+    
+    BalancingOrder bo1t1 = new BalancingOrder(b1, b1ts1, 0.8, 0.2);
+    tariffRepo.addBalancingOrder(bo1t1);
+    BalancingOrder bo1t2 = new BalancingOrder(b1, b1ts2, 0.6, 0.15);
+    tariffRepo.addBalancingOrder(bo1t2);
+    BalancingOrder bo2t1 = new BalancingOrder(b2, b2ts1, 0.7, 0.1);
+    tariffRepo.addBalancingOrder(bo2t1);
+    
+    assertEquals("correct number of bo", 3,
+                 tariffRepo.getBalancingOrders().size());
+
+    Map<Broker, ChargeInfo> chargeInfos =
+            balancingMarketService.balanceTimeslot(timeslotRepo.currentTimeslot(),
+                                                       brokerList);
+    assertEquals("correct count", 3, chargeInfos.size());
+    
+    ChargeInfo c1b1 = findFirst(chargeInfos.values(),
+                                new Predicate<ChargeInfo>() {
+      @Override
+      public boolean apply (ChargeInfo item)
+      {
+        return (item.getBroker() == b1);
+      }
+    });
+    assertNotNull("found correct chargeInfo", c1b1);
+    List<BalancingOrder> orders = c1b1.getBalancingOrders();
+    assertEquals("found 2 balancing orders", 2, orders.size());
+    assertTrue("contains bo1t1", orders.contains(bo1t1));
+    assertTrue("contains bo1t2", orders.contains(bo1t2));
+    
+    ChargeInfo c1b2 = findFirst(chargeInfos.values(),
+                                new Predicate<ChargeInfo>() {
+      @Override
+      public boolean apply (ChargeInfo item)
+      {
+        return (item.getBroker() == b2);
+      }
+    });
+    assertNotNull("found correct chargeInfo", c1b2);
+    orders = c1b2.getBalancingOrders();
+    assertEquals("found 1 balancing order", 1, orders.size());
+    assertTrue("contains bo2t1", orders.contains(bo2t1));
+  }
+}
