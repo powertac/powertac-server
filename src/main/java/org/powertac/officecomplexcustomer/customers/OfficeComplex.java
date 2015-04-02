fa75f2c78229b0feee0fabcfd90891e7a424a5ab
John Collins <jcollins@cs.umn.edu>
2015-04-01 21:58:42 -0500

    small bugfix from Govert

--- src/main/java/org/powertac/officecomplexcustomer/customers/OfficeComplex.java
+++ src/main/java/org/powertac/officecomplexcustomer/customers/OfficeComplex.java
@@ -899,10 +899,8 @@ public class OfficeComplex extends AbstractCustomer
       log.debug("Consumption Load for Customer " + customer.toString() + ": "
                 + load + " for subscriptions " + subscriptions.toString());
 
-      if (subscriptions != null || subscriptions.size() != 0) {
-
+      if (subscriptions != null && subscriptions.size() != 0) {
         subscriptions.get(0).usePower(load);
-
       }
 
     }
