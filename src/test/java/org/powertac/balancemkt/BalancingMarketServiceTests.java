79a9308ff2a3f3dc9c54c573b75626575ad30da4
Erik Kemperman <erikkemperman@gmail.com>
2015-12-09 16:04:23 +0100

    Remove unused imports

--- src/test/java/org/powertac/balancemkt/BalancingMarketServiceTests.java
+++ src/test/java/org/powertac/balancemkt/BalancingMarketServiceTests.java
@@ -45,7 +45,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:test-config.xml" })
