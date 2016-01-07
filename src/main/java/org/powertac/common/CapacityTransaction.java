9965be741c3db1f59cf8c4fe55ccf98269a2b3f9
John Collins <jcollins@cs.umn.edu>
2016-01-07 17:26:53 -0600

    #840 - fix msg label

--- src/main/java/org/powertac/common/CapacityTransaction.java
+++ src/main/java/org/powertac/common/CapacityTransaction.java
@@ -29,7 +29,7 @@ import com.thoughtworks.xstream.annotations.XStreamAsAttribute;
  */
 @Domain(fields = {"postedTimeslot", "peakTimeslot",
                   "threshold", "KWh", "charge"})
-@XStreamAlias("distribution-tx")
+@XStreamAlias("capacity-tx")
 public class CapacityTransaction extends BrokerTransaction
 {
   @XStreamAsAttribute
