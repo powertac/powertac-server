d97fd023b74a56cb7c6d49c535e823e5e72235bb
John Collins <jcollins@cs.umn.edu>
2016-03-05 18:46:27 -0600

    #854 - add Battery model

---
+++ src/main/java/org/powertac/customer/model/Battery.java
@@ -0,0 +1,358 @@
+/*
+ * Copyright (c) 2016 by John E. Collins
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.powertac.customer.model;
+
+import java.util.List;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.joda.time.Instant;
+import org.powertac.common.CapacityProfile;
+import org.powertac.common.Competition;
+import org.powertac.common.CustomerInfo;
+import org.powertac.common.CustomerInfo.CustomerClass;
+import org.powertac.common.RandomSeed;
+import org.powertac.common.RegulationCapacity;
+import org.powertac.common.Tariff;
+import org.powertac.common.TariffEvaluator;
+import org.powertac.common.TariffSubscription;
+import org.powertac.common.TimeService;
+import org.powertac.common.config.ConfigurableInstance;
+import org.powertac.common.config.ConfigurableValue;
+import org.powertac.common.enumerations.PowerType;
+import org.powertac.common.interfaces.CustomerModelAccessor;
+import org.powertac.common.state.Domain;
+import org.powertac.common.state.StateChange;
+import org.powertac.customer.AbstractCustomer;
+
+/**
+ * Models a single grid-connected storage battery with configurable capacity,
+ * max charge rate, max discharge rate, and efficiency. Batteries do not charge
+ * unless used for regulation, so repeated use for up-regulation will leave
+ * them discharged, and repeated use for down-regulation will leave them
+ * fully charged. If not used at all, they will eventually end up completely
+ * discharged just in case their selfDischargeRate values are non-zero.
+ * The initial state-of-charge is always 0.0;
+ * 
+ * @author John Collins
+ */
+@Domain
+@ConfigurableInstance
+public class Battery
+extends AbstractCustomer
+implements CustomerModelAccessor
+{
+  static private Logger log = LogManager.getLogger(Battery.class.getName());
+
+  // configured through setters...
+  private double capacityKWh = 50.0;
+  private double maxChargeKW = 20.0;
+  private double maxDischargeKW = -20.0;
+  private double chargeEfficiency = 0.95;
+  private double selfDischargeRate = 0.0005; // Proportion of capacity lost/hour
+
+  // current State of Charge
+  @ConfigurableValue(valueType = "Double",
+      bootstrapState = true,
+      description = "State of charge")
+  private double stateOfCharge = 0.0; // current energy content
+
+  private PowerType powerType = PowerType.BATTERY_STORAGE;
+
+  // random seeds
+  private RandomSeed evalSeed = null;
+
+  // context references
+  private TariffEvaluator tariffEvaluator;
+
+  /**
+   * Default constructor, requires manual setting of name
+   */
+  public Battery ()
+  {
+    super();
+  }
+
+  /**
+   * Standard constructor for named configurable type
+   */
+  public Battery (String name)
+  {
+    super(name);
+  }
+
+  /**
+   * Initialization must provide accessor to Customer instance and time.
+   * We assume configuration has already happened. We start with the battery
+   * at its targetSOC. 
+   */
+  @Override
+  public void initialize ()
+  {
+    super.initialize();
+    log.info("Initialize " + name);
+    // fill out CustomerInfo.
+    CustomerInfo info = new CustomerInfo(name, 1);
+    // conservative interruptible capacity
+    double interruptible = maxChargeKW;
+    info.withPowerType(powerType)
+        .withCustomerClass(CustomerClass.LARGE)
+        .withControllableKW(-interruptible)
+        .withStorageCapacity(capacityKWh)
+        .withUpRegulationKW(maxDischargeKW)
+        .withDownRegulationKW(maxChargeKW);
+    addCustomerInfo(info);
+    ensureSeeds();
+
+    // validate parameters
+    if (capacityKWh <= 0.0) {
+      log.error("{}: bad capacity value {}",
+                name, capacityKWh);
+      capacityKWh = 1.0;
+    }
+    if (maxChargeKW <= 0.0) {
+      log.error("{}: bad value {} for maxChargeKW",
+                name, maxChargeKW);
+      maxChargeKW = 1.0;
+    }
+    if (maxDischargeKW >= 0.0) {
+      log.error("{}: bad value {} for maxDischargeKW",
+                name, maxDischargeKW);
+      maxDischargeKW = -1.0;
+    }
+    if (selfDischargeRate < 0.0 || selfDischargeRate > 1.0) {
+      log.error ("{}: selfDischargeRate {} invalid",
+                 name, selfDischargeRate);
+      selfDischargeRate = 0.0;
+    }
+    if (chargeEfficiency < 0.0 || chargeEfficiency > 1.0) {
+      log.error ("{}: chargeEfficiency {} invalid",
+                 name, chargeEfficiency);
+      chargeEfficiency = 1.0;
+    }
+
+    // set up the tariff evaluator. We are wide-open to variable pricing.
+    tariffEvaluator = new TariffEvaluator(this);
+    tariffEvaluator.withInertia(0.7).withPreferredContractDuration(14);
+    tariffEvaluator.initializeInconvenienceFactors(0.0, 0.01, 0.0, 0.0);
+    tariffEvaluator.initializeRegulationFactors(-maxChargeKW * 0.5,
+                                                maxDischargeKW * 0.5,
+                                                maxChargeKW * 0.5);
+  }
+
+  // Gets a new random-number seeds just in case we don't already have them.
+  // Useful for mock-based testing.
+  private void ensureSeeds ()
+  {
+    if (null == evalSeed) {
+      evalSeed = service.getRandomSeedRepo()
+          .getRandomSeed(Battery.class.getName() + "-" + name,
+                         0, "eval");
+    }
+  }
+
+  @Override
+  public CustomerInfo getCustomerInfo ()
+  {
+    return getCustomerInfo(powerType);
+  }
+
+  // ======== per-timeslot activities ========
+  @Override
+  public void step ()
+  {
+    TariffSubscription subscription = getSubscription();
+
+    // use energy on chargers, accounting for regulation
+    // up-regulation is positive
+    double regulation = subscription.getRegulation();
+    log.info(getName() + ": regulation " + regulation);
+    if (regulation > 0) {
+      // up-regulation reduces state-of-charge
+      stateOfCharge -= regulation;
+    }
+    else {
+      // down-regulation increases state-of-charge
+      stateOfCharge -= regulation * chargeEfficiency;
+    }
+
+    // self-discharge
+    double timeslotLength = // in hours
+        (Competition.currentCompetition().getTimeslotDuration()
+                 / (double) TimeService.HOUR);
+    stateOfCharge = Math.max(0.0,
+                             stateOfCharge
+                             - (selfDischargeRate * capacityKWh
+                                 * timeslotLength));
+
+    // set regulation capacity for next timeslot
+    double up = Math.min(stateOfCharge, -maxDischargeKW * timeslotLength);
+    double down = -((Math.min((capacityKWh - stateOfCharge) / chargeEfficiency,
+                              maxChargeKW * timeslotLength)));
+    subscription.setRegulationCapacity(new RegulationCapacity(subscription,
+                                                              up, down));
+  }
+
+  // ================ getters and setters =====================
+  // Note that list values must arrive and depart as List<String>,
+  // while internally many of them are lists or arrays of numeric values.
+  // Therefore we provide @ConfigurableValue setters that do the translation.
+  @Override
+  public String getName ()
+  {
+    return name;
+  }
+
+  @Override
+  public void setName (String name)
+  {
+    this.name = name;
+  }
+
+  //private double capacityKWh = 50.0;
+  @ConfigurableValue(valueType = "Double",
+      description = "size of battery in kWh")
+  @StateChange
+  public void setCapacityKWh (double value)
+  {
+    capacityKWh = value;
+  }
+
+  public double getCapacityKWh ()
+  {
+    return capacityKWh;
+  }
+
+  //private double maxChargeKW = 5.0;
+  @ConfigurableValue(valueType = "Double",
+      description = "maximum charge rate")
+  @StateChange
+  public void setMaxChargeKW (double value)
+  {
+    maxChargeKW = value;
+  }
+
+  public double getMaxChargeKW ()
+  {
+    return maxChargeKW;
+  }
+
+  //private double maxDischargeKW = 5.0;
+  @ConfigurableValue(valueType = "Double",
+      description = "Maximum discharge rate")
+  public void setMaxDischargeKW (double value)
+  {
+    maxDischargeKW = value;
+  }
+
+  public double getMaxDischargeKW ()
+  {
+    return maxDischargeKW;
+  }
+
+//private double chargeEfficiency = 0.9;
+  @ConfigurableValue(valueType = "Double",
+      description = "ratio of charge energy to battery energy")
+  @StateChange
+  public void setChargeEfficiency (double value)
+  {
+    chargeEfficiency = value;
+  }
+
+  public double getChargeEfficiency ()
+  {
+    return chargeEfficiency;
+  }
+
+  //private double selfDischargeRate = 0.001;
+  @ConfigurableValue(valueType = "Double",
+      description = "hourly charge lost as proportion of capacity")
+  public void setSelfDischargeRate (double value)
+  {
+    selfDischargeRate = value;
+  }
+
+  public double getSelfDischargeRate ()
+  {
+    return selfDischargeRate;
+  }
+
+  // =========== Test support ============
+  double getStateOfCharge ()
+  {
+    return stateOfCharge;
+  }
+
+  void setStateOfCharge (double value)
+  {
+    stateOfCharge = value;
+  }
+
+  // ======== CustomerModelAccessor API ===========
+
+  // digs out the current subscription for this thing. Since the population is
+  // always one, there should only ever be one of them
+  private TariffSubscription getSubscription ()
+  {
+    List<TariffSubscription> subs = getCurrentSubscriptions(powerType);
+    if (subs.size() > 1) {
+      log.warn("Multiple subscriptions " + subs.size() + " for " + getName());
+    }
+    return subs.get(0);
+  }
+
+  @Override
+  public double getBrokerSwitchFactor (boolean isSuperseding)
+  {
+    if (isSuperseding)
+      return 0;
+    else
+      return 0.02;
+  }
+
+  @Override
+  public double getTariffChoiceSample ()
+  {
+    return evalSeed.nextDouble();
+  }
+
+  @Override
+  public double getInertiaSample ()
+  {
+    return evalSeed.nextDouble();
+  }
+
+  @Override
+  public void evaluateTariffs (List<Tariff> tariffs)
+  {
+    log.info(getName() + ": evaluate tariffs");
+    tariffEvaluator.evaluateTariffs();
+  }
+
+  @Override
+  public double getShiftingInconvenienceFactor(Tariff tariff) {
+    return 0;
+  }
+
+  @Override
+  public CapacityProfile getCapacityProfile (Tariff tariff)
+  {
+    Instant start =
+        service.getTimeslotRepo().currentTimeslot().getStartInstant();
+    return new CapacityProfile (new double[] {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0},
+                                start);
+  }
+}
---
+++ src/test/java/org/powertac/customer/model/BatteryTest.java
@@ -0,0 +1,319 @@
+/*
+ * Copyright (c) 2016 by the original author
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.powertac.customer.model;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyObject;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.MapConfiguration;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.Instant;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.powertac.common.Broker;
+import org.powertac.common.Competition;
+import org.powertac.common.RandomSeed;
+import org.powertac.common.Rate;
+import org.powertac.common.RegulationCapacity;
+import org.powertac.common.Tariff;
+import org.powertac.common.TariffSpecification;
+import org.powertac.common.TariffSubscription;
+import org.powertac.common.TimeService;
+import org.powertac.common.config.Configurator;
+import org.powertac.common.enumerations.PowerType;
+import org.powertac.common.interfaces.CustomerServiceAccessor;
+import org.powertac.common.interfaces.ServerConfiguration;
+import org.powertac.common.repo.CustomerRepo;
+import org.powertac.common.repo.RandomSeedRepo;
+import org.powertac.common.repo.TariffRepo;
+import org.powertac.common.repo.TariffSubscriptionRepo;
+import org.powertac.common.repo.TimeslotRepo;
+import org.powertac.common.repo.WeatherReportRepo;
+import org.powertac.customer.model.LiftTruckTest.ServiceAccessor;
+import org.springframework.test.util.ReflectionTestUtils;
+
+/**
+ * @author jcollins
+ */
+public class BatteryTest
+{
+  private Competition competition;
+  private TimeService timeService;
+  private TariffRepo tariffRepo;
+  private TariffSubscriptionRepo mockSubscriptionRepo;
+  private Broker broker;
+  private TariffSpecification spec;
+  private Tariff tariff;
+  private TariffSubscription subscription;
+  private RandomSeedRepo mockSeedRepo;
+  private RandomSeed seed;
+  private ServerConfiguration serverConfig;
+  private Configurator configurator;
+  private Configuration config;
+  private TimeslotRepo tsRepo;
+  private ServiceAccessor serviceAccessor;
+
+  @Before
+  public void setUp () throws Exception
+  {
+    tsRepo = mock(TimeslotRepo.class);
+    competition =
+        Competition.newInstance("ColdStorage test").withTimeslotsOpen(4);
+    Competition.setCurrent(competition);
+    timeService = new TimeService();
+    Instant now =
+        new DateTime(2011, 1, 10, 0, 0, 0, 0, DateTimeZone.UTC).toInstant();
+    timeService.setCurrentTime(now);
+
+    // tariff setup
+    tariffRepo = new TariffRepo();
+    broker = new Broker("Sam");
+    spec =
+        new TariffSpecification(broker, PowerType.THERMAL_STORAGE_CONSUMPTION)
+    .addRate(new Rate().withValue(-0.11));
+    tariff = new Tariff(spec);
+    ReflectionTestUtils.setField(tariff, "timeService", timeService);
+    ReflectionTestUtils.setField(tariff, "tariffRepo", tariffRepo);
+    tariff.init();
+
+    // need a subscription
+    subscription = mock(TariffSubscription.class);
+    List<TariffSubscription> subs = new ArrayList<TariffSubscription>();
+    subs.add(subscription);
+    mockSubscriptionRepo = mock(TariffSubscriptionRepo.class);
+    when(mockSubscriptionRepo.findActiveSubscriptionsForCustomer(anyObject()))
+        .thenReturn(subs);
+
+    // set up randomSeed mock
+    mockSeedRepo = mock(RandomSeedRepo.class);
+    seed = mock(RandomSeed.class);
+    when(mockSeedRepo.getRandomSeed(anyString(),
+                                    anyInt(),
+                                    anyString())).thenReturn(seed);
+
+    // Set up serverProperties mock
+    serverConfig = mock(ServerConfiguration.class);
+    configurator = new Configurator();
+    doAnswer(new Answer<Object>() {
+      @Override
+      public Object answer(InvocationOnMock invocation) {
+        Object[] args = invocation.getArguments();
+        configurator.configureSingleton(args[0]);
+        return null;
+      }
+    }).when(serverConfig).configureMe(anyObject());
+
+    serviceAccessor = new ServiceAccessor();
+  }
+
+  @Test
+  public void testCreate ()
+  {
+    Battery battery = new Battery("test");
+    assertNotNull("constructed", battery);
+    assertEquals("correct capacity", 50.0, battery.getCapacityKWh(), 1e-6);
+  }
+
+  @Test
+  public void testConfig ()
+  {
+    TreeMap<String, String> map = new TreeMap<String, String>();
+    map.put("customer.model.battery.instances",
+            "b1, b2");
+    map.put("customer.model.battery.b1.capacityKWh", "30.0");
+    map.put("customer.model.battery.b2.capacityKWh", "60.0");
+    map.put("customer.model.battery.b1.maxChargeKW", "10.0");
+    map.put("customer.model.battery.b2.maxChargeKW", "4.0");
+    map.put("customer.model.battery.b1.maxDischargeKW", "-12.0");
+    map.put("customer.model.battery.b2.maxDischargeKW", "-6.0");
+    config = new MapConfiguration(map);
+    Configurator configurator = new Configurator();
+    configurator.setConfiguration(config);
+    Collection<?> instances =
+        configurator.configureInstances(Battery.class);
+    assertEquals("two instances", 2, instances.size());
+    Map<String, Battery> batteries = mapNames(instances);
+    Battery b1 = batteries.get("b1");
+    assertNotNull("Found b1", b1);
+    assertEquals("correct capacity", 30.0, b1.getCapacityKWh(), 1e-6);
+    assertEquals("correct charge", 10.0, b1.getMaxChargeKW(), 1e-6);
+    assertEquals("correct discharge", -12.0, b1.getMaxDischargeKW(), 1e-6);
+    Battery b2 = batteries.get("b2");
+    assertNotNull("Found b2", b2);
+    assertEquals("correct capacity", 60.0, b2.getCapacityKWh(), 1e-6);
+    assertEquals("correct charge", 4.0, b2.getMaxChargeKW(), 1e-6);
+    assertEquals("correct discharge", -6.0, b2.getMaxDischargeKW(), 1e-6);
+  }
+
+  @Test
+  public void testBogusConfig ()
+  {
+    TreeMap<String, String> map = new TreeMap<String, String>();
+    map.put("customer.model.battery.instances",
+            "b1, b2");
+    map.put("customer.model.battery.b1.capacityKWh", "0.0");
+    map.put("customer.model.battery.b2.capacityKWh", "-6.0");
+    map.put("customer.model.battery.b1.maxChargeKW", "0.0");
+    map.put("customer.model.battery.b2.maxChargeKW", "-4.0");
+    map.put("customer.model.battery.b1.maxDischargeKW", "-0.0");
+    map.put("customer.model.battery.b2.maxDischargeKW", "6.0");
+    map.put("customer.model.battery.b1.selfDischargeRate", "-0.2");
+    map.put("customer.model.battery.b2.selfDischargeRate", "1.2");
+    map.put("customer.model.battery.b1.chargeEfficiency", "-0.2");
+    map.put("customer.model.battery.b2.chargeEfficiency", "1.1");
+    config = new MapConfiguration(map);
+    Configurator configurator = new Configurator();
+    configurator.setConfiguration(config);
+    Collection<?> instances =
+        configurator.configureInstances(Battery.class);
+    assertEquals("two instances", 2, instances.size());
+    Map<String, Battery> batteries = mapNames(instances);
+    Battery b1 = batteries.get("b1");
+    b1.setServiceAccessor(serviceAccessor);
+    b1.initialize();
+    Battery b2 = batteries.get("b2");
+    b2.setServiceAccessor(serviceAccessor);
+    b2.initialize();
+    assertEquals("fixed capacity", 1.0, b1.getCapacityKWh(), 1e-6);
+    assertEquals("fixed capacity", 1.0, b2.getCapacityKWh(), 1e-6);
+    assertEquals("fixed charge rate", 1.0, b1.getMaxChargeKW(), 1e-6);
+    assertEquals("fixed charge rate", 1.0, b2.getMaxChargeKW(), 1e-6);
+    assertEquals("fixed discharge rate", -1.0, b1.getMaxDischargeKW(), 1e-6);
+    assertEquals("fixed discharge rate", -1.0, b2.getMaxDischargeKW(), 1e-6);
+    assertEquals("fixed sdr", 0.0, b1.getSelfDischargeRate(), 1e-6);
+    assertEquals("fixed sdr", 0.0, b2.getSelfDischargeRate(), 1e-6);
+    assertEquals("fixed ce", 1.0, b1.getChargeEfficiency(), 1e-6);
+    assertEquals("fixed ce", 1.0, b2.getChargeEfficiency(), 1e-6);
+  }
+
+  @Test
+  public void testRC1 ()
+  {
+    Battery battery = new Battery("rc1");
+    battery.setServiceAccessor(serviceAccessor);
+    battery.initialize();
+    when(subscription.getRegulation()).thenReturn(-5.0);
+    battery.step();
+    assertEquals("correct soc", 4.725, battery.getStateOfCharge(), 1e-6);
+    ArgumentCaptor<RegulationCapacity> arg =
+        ArgumentCaptor.forClass(RegulationCapacity.class);
+    verify(subscription).setRegulationCapacity(arg.capture());
+    RegulationCapacity rc = arg.getValue();
+    assertNotNull(rc);
+    assertEquals("correct up", 4.725, rc.getUpRegulationCapacity(), 1e-6);
+    assertEquals("correct down", -20.0, rc.getDownRegulationCapacity(), 1e-6);
+  }
+
+  @Test
+  public void testRC2 ()
+  {
+    Battery battery = new Battery("rc1");
+    battery.setServiceAccessor(serviceAccessor);
+    battery.initialize();
+    when(subscription.getRegulation()).thenReturn(-25.0);
+    battery.step();
+    assertEquals("correct soc", 23.725, battery.getStateOfCharge(), 1e-6);
+    ArgumentCaptor<RegulationCapacity> arg =
+        ArgumentCaptor.forClass(RegulationCapacity.class);
+    verify(subscription).setRegulationCapacity(arg.capture());
+    RegulationCapacity rc = arg.getValue();
+    assertNotNull(rc);
+    assertEquals("correct up", 20.0, rc.getUpRegulationCapacity(), 1e-6);
+    assertEquals("correct down", -20.0, rc.getDownRegulationCapacity(), 1e-6);
+  }
+
+  // map names to instances
+  private Map<String, Battery> mapNames (Collection<?> objects)
+  {
+    Map<String, Battery> result = new HashMap<String, Battery>();
+    for (Object thing : objects) {
+      Battery bt = (Battery)thing;
+      result.put(bt.getName(), bt);
+    }
+    return result;
+  }
+
+  class ServiceAccessor implements CustomerServiceAccessor
+  {
+
+    @Override
+    public CustomerRepo getCustomerRepo ()
+    {
+      return null;
+    }
+
+    @Override
+    public RandomSeedRepo getRandomSeedRepo ()
+    {
+      return mockSeedRepo;
+    }
+
+    @Override
+    public TariffRepo getTariffRepo ()
+    {
+      return tariffRepo;
+    }
+
+    @Override
+    public TariffSubscriptionRepo getTariffSubscriptionRepo ()
+    {
+      return mockSubscriptionRepo;
+    }
+
+    @Override
+    public TimeslotRepo getTimeslotRepo ()
+    {
+      return tsRepo;
+    }
+
+    @Override
+    public TimeService getTimeService ()
+    {
+      return timeService;
+    }
+
+    @Override
+    public WeatherReportRepo getWeatherReportRepo ()
+    {
+      return null;
+    }
+
+    @Override
+    public ServerConfiguration getServerConfiguration ()
+    {
+      // Auto-generated method stub
+      return null;
+    }
+  }
+}
