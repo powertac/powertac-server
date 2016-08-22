4b9def525b81322e7ae38f51dff4b2dea1381e4a
Erik Kemperman <erikkemperman@gmail.com>
2016-08-22 16:54:15 +0200

    Ensure consistent color assignments

--- src/main/webapp/app/graphs/graphs.controller.js
+++ src/main/webapp/app/graphs/graphs.controller.js
@@ -49,45 +49,55 @@
             vm.subscriptionsConfig = angular.copy(chartConfig);
             vm.subscriptionCumulativesConfig = angular.copy(chartConfig);
 
-            State.brokers.forEach(function (broker) {
+            var colors = ['#7cb5ec', '#434348', '#90ed7d', '#f7a35c', '#8085e9',
+                '#f15c80', '#e4d354', '#2b908f', '#f45b5b', '#91e8e1'];
+            State.brokers.forEach(function (broker, index) {
+                var color = colors[index % colors.length];
                 vm.allMoneyCumulativesConfig.series.push({
                     name: broker.name,
+                    color: color,
                     data: broker.graphData.allMoneyCumulative,
                     pointStart: vm.state.competition.simulationBaseTime.millis,
                     pointInterval: vm.state.competition.timeslotDuration // one day
                 });
                 vm.retailMoneyCumulativesConfig.series.push({
                     name: broker.name,
+                    color: color,
                     data: broker.graphData.retailMoneyCumulative,
                     pointStart: vm.state.competition.simulationBaseTime.millis,
                     pointInterval: vm.state.competition.timeslotDuration // one day
                 });
                 vm.retailMoneysConfig.series.push({
                     name: broker.name,
+                    color: color,
                     data: broker.graphData.retailMoney,
                     pointStart: vm.state.competition.simulationBaseTime.millis,
                     pointInterval: vm.state.competition.timeslotDuration // one day
                 });
                 vm.retailKwhCumulativesConfig.series.push({
                     name: broker.name,
+                    color: color,
                     data: broker.graphData.retailKwhCumulative,
                     pointStart: vm.state.competition.simulationBaseTime.millis,
                     pointInterval: vm.state.competition.timeslotDuration // one day
                 });
                 vm.retailKwhsConfig.series.push({
                     name: broker.name,
+                    color: color,
                     data: broker.graphData.retailKwh,
                     pointStart: vm.state.competition.simulationBaseTime.millis,
                     pointInterval: vm.state.competition.timeslotDuration // one day
                 });
                 vm.subscriptionsConfig.series.push({
                     name: broker.name,
+                    color: color,
                     data: broker.graphData.subscription,
                     pointStart: vm.state.competition.simulationBaseTime.millis,
                     pointInterval: vm.state.competition.timeslotDuration // one day
                 });
                 vm.subscriptionCumulativesConfig.series.push({
                     name: broker.name,
+                    color: color,
                     data: broker.graphData.subscriptionCumulative,
                     pointStart: vm.state.competition.simulationBaseTime.millis,
                     pointInterval: vm.state.competition.timeslotDuration // one day
