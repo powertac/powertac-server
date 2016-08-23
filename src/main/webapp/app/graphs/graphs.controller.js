e2f8288ed1b2e05c0ee75f5e1a670e0fa166b0f0
Erik Kemperman <erikkemperman@gmail.com>
2016-08-23 13:09:15 +0200

    Fix offset error in x-axis legends

--- src/main/webapp/app/graphs/graphs.controller.js
+++ src/main/webapp/app/graphs/graphs.controller.js
@@ -51,56 +51,66 @@
 
             var colors = ['#7cb5ec', '#434348', '#90ed7d', '#f7a35c', '#8085e9',
                 '#f15c80', '#e4d354', '#2b908f', '#f45b5b', '#91e8e1'];
+            if (vm.state.competition && vm.state.competition.simulationBaseTime) {
+                vm.duration = vm.state.competition.timeslotDuration; // one hour
+                vm.start = vm.state.competition.simulationBaseTime.millis;
+                // Uggh.. Detect boot game by number of brokers
+                if (State.brokers.length > 1) {
+                    vm.start += vm.duration * (vm.state.competition.bootstrapTimeslotCount + vm.state.competition.bootstrapDiscardedTimeslots);
+                }
+            }
             State.brokers.forEach(function (broker, index) {
                 var color = colors[index % colors.length];
+
+
                 vm.allMoneyCumulativesConfig.series.push({
                     name: broker.name,
                     color: color,
                     data: broker.graphData.allMoneyCumulative,
-                    pointStart: vm.state.competition.simulationBaseTime.millis,
-                    pointInterval: vm.state.competition.timeslotDuration // one day
+                    pointStart: vm.start,
+                    pointInterval: vm.duration
                 });
                 vm.retailMoneyCumulativesConfig.series.push({
                     name: broker.name,
                     color: color,
                     data: broker.graphData.retailMoneyCumulative,
-                    pointStart: vm.state.competition.simulationBaseTime.millis,
-                    pointInterval: vm.state.competition.timeslotDuration // one day
+                    pointStart: vm.start,
+                    pointInterval: vm.duration
                 });
                 vm.retailMoneysConfig.series.push({
                     name: broker.name,
                     color: color,
                     data: broker.graphData.retailMoney,
-                    pointStart: vm.state.competition.simulationBaseTime.millis,
-                    pointInterval: vm.state.competition.timeslotDuration // one day
+                    pointStart: vm.start,
+                    pointInterval: vm.duration
                 });
                 vm.retailKwhCumulativesConfig.series.push({
                     name: broker.name,
                     color: color,
                     data: broker.graphData.retailKwhCumulative,
-                    pointStart: vm.state.competition.simulationBaseTime.millis,
-                    pointInterval: vm.state.competition.timeslotDuration // one day
+                    pointStart: vm.start,
+                    pointInterval: vm.duration
                 });
                 vm.retailKwhsConfig.series.push({
                     name: broker.name,
                     color: color,
                     data: broker.graphData.retailKwh,
-                    pointStart: vm.state.competition.simulationBaseTime.millis,
-                    pointInterval: vm.state.competition.timeslotDuration // one day
+                    pointStart: vm.start,
+                    pointInterval: vm.duration
                 });
                 vm.subscriptionsConfig.series.push({
                     name: broker.name,
                     color: color,
                     data: broker.graphData.subscription,
-                    pointStart: vm.state.competition.simulationBaseTime.millis,
-                    pointInterval: vm.state.competition.timeslotDuration // one day
+                    pointStart: vm.start,
+                    pointInterval: vm.duration
                 });
                 vm.subscriptionCumulativesConfig.series.push({
                     name: broker.name,
                     color: color,
                     data: broker.graphData.subscriptionCumulative,
-                    pointStart: vm.state.competition.simulationBaseTime.millis,
-                    pointInterval: vm.state.competition.timeslotDuration // one day
+                    pointStart: vm.start,
+                    pointInterval: vm.duration
                 });
             });
         }
