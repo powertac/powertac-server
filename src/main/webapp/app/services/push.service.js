89e3fa604497b8dbc1b2e833a711e7957d701d70
Erik Kemperman <erikkemperman@gmail.com>
2016-10-27 10:47:41 +0200

    Fix websocket reconnect

--- src/main/webapp/app/services/push.service.js
+++ src/main/webapp/app/services/push.service.js
@@ -15,7 +15,11 @@
         var loc = window.location;
         var SOCKET_URL = '//' + loc.host + loc.pathname + 'websocket/push';
         var TOPIC = '/topic/push';
-        var RECONNECT_TIMEOUT = 30000;
+
+        var RECONNECT_TIMEOUT_START = 500;
+        var RECONNECT_TIMEOUT_LIMIT = 32000;
+        var delay = RECONNECT_TIMEOUT_START;
+        var offset = Math.ceil(Math.random() * RECONNECT_TIMEOUT_START)
 
         var listener = $q.defer();
         var socket = {
@@ -27,29 +31,31 @@
             return listener.promise;
         }
 
-        function reconnect () {
-            $log.info('reconnect');
-            $timeout(function () {
-                initialize();
-            }, RECONNECT_TIMEOUT);
-        }
-
         function getMessage (data) {
             return JSON.parse(data);
         }
 
-        function startListener () {
+        function success () {
             // new values
+            delay = RECONNECT_TIMEOUT_START
             socket.stomp.subscribe(TOPIC, function (data) {
                 listener.notify(getMessage(data.body));
             });
         }
 
+        function failure (error) {
+            $log.debug('STOMP error ' + error);
+            $log.debug('reconnecting in ~' + delay + ' ms.');
+            $timeout(function () {
+                delay = Math.min(2 * delay, RECONNECT_TIMEOUT_LIMIT)
+                initialize();
+            }, delay + offset);
+        }
+
         function initialize () {
             socket.client = new SockJS(SOCKET_URL);
             socket.stomp = Stomp.over(socket.client);
-            socket.stomp.connect({}, startListener);
-            socket.stomp.onclose = reconnect;
+            socket.stomp.connect({}, success, failure);
             if (!DEBUG_INFO_ENABLED) {
                 socket.stomp.debug = null;
             }
