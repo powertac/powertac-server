417452e4eb6bcc16cac5dc2b11b79f12f93831a1
Pascal Brokmeier <accounts@pascalbrokmeier.de>
2018-02-26 22:35:12 +0100

    correcting 4 spaces 2 spaces

--- broker-core/src/main/java/org/powertac/samplebroker/core/BrokerMessageReceiver.java
+++ broker-core/src/main/java/org/powertac/samplebroker/core/BrokerMessageReceiver.java
@@ -45,111 +45,111 @@ import org.springframework.stereotype.Service;
 @Service
 public class BrokerMessageReceiver implements MessageListener
 {
-    static private Logger log = LogManager.getLogger(BrokerMessageReceiver.class);
+  static private Logger log = LogManager.getLogger(BrokerMessageReceiver.class);
 
-    @Autowired
-    private XMLMessageConverter converter;
+  @Autowired
+  private XMLMessageConverter converter;
 
-    @Autowired
-    private MessageDispatcher messageDispatcher;
+  @Autowired
+  private MessageDispatcher messageDispatcher;
 
-    @Autowired
-    private BrokerPropertiesService propertiesService;
+  @Autowired
+  private BrokerPropertiesService propertiesService;
 
-    private IpcAdapter adapter;
+  private IpcAdapter adapter;
 
-    @ConfigurableValue(valueType = "Boolean",
-            description = "If true, then some messages are not converted to java")
-    private Boolean rawXml = false;
+  @ConfigurableValue(valueType = "Boolean",
+      description = "If true, then some messages are not converted to java")
+  private Boolean rawXml = false;
 
-    @ConfigurableValue(valueType = "List",
-            description = "These xml message types are passed without conversion")
-    private List<String> rawMsgTypes = new ArrayList<>();
+  @ConfigurableValue(valueType = "List",
+      description = "These xml message types are passed without conversion")
+  private List<String> rawMsgTypes = new ArrayList<>();
 
-    @ConfigurableValue(valueType = "List",
-            description = "These xml message types are passed after conversion")
-    private List<String> cookedMsgTypes = new ArrayList<>();
+  @ConfigurableValue(valueType = "List",
+      description = "These xml message types are passed after conversion")
+  private List<String> cookedMsgTypes = new ArrayList<>();
 
-    @ConfigurableValue(valueType = "String", description = "Type of xml forwarding. Can be 'CharStreamAdapter' or 'GrpcSocketAdapter'")
-    private String xmlForwardType = "CharStreamAdapter";
+  @ConfigurableValue(valueType = "String", description = "Type of xml forwarding. Can be 'CharStreamAdapter' or 'GrpcSocketAdapter'")
+  private String xmlForwardType = "CharStreamAdapter";
 
-    // hash sets to speed lookup of xml types
-    private HashSet<String> rawTypes;
-    private HashSet<String> cookedTypes;
-    private Pattern tagRe = Pattern.compile("<([-_\\w]+)[\\s/>]");
+  // hash sets to speed lookup of xml types
+  private HashSet<String> rawTypes;
+  private HashSet<String> cookedTypes;
+  private Pattern tagRe = Pattern.compile("<([-_\\w]+)[\\s/>]");
 
-    public void initialize()
-    {
-        propertiesService.configureMe(this);
+  public void initialize()
+  {
+    propertiesService.configureMe(this);
+    if (rawXml) {
+      log.info("rawXml={}", rawXml);
+      // set up data structures
+      rawTypes = new HashSet<>();
+      rawMsgTypes.forEach(msg -> {
+        //log.info("raw type {}", msg);
+        rawTypes.add(msg);
+      });
+      cookedTypes = new HashSet<>();
+      cookedMsgTypes.forEach(msg -> {
+        //log.info("cooked type {}", msg);
+        cookedTypes.add(msg);
+      });
+      // find the message handler if it's not already there
+      setMessageAdapter();
+    }
+  }
+
+  private void setMessageAdapter()
+  {
+    if (null == adapter) {
+      adapter = (IpcAdapter) SpringApplicationContext.getBean(xmlForwardType);
+    }
+    if (null == adapter) {
+      log.error("Raw xml specified, but no adapter available");
+      rawXml = false;
+    }
+    log.info("Using {} for xml forwarding", xmlForwardType);
+  }
+
+
+  @Override
+  public void onMessage(Message message)
+  {
+    if (message instanceof TextMessage) {
+      String msg;
+      try {
+        log.debug("onMessage(Message) - receiving a message");
+        msg = ((TextMessage) message).getText();
+        log.info("received message:\n" + msg);
+        //onMessage(msg);
         if (rawXml) {
-            log.info("rawXml={}", rawXml);
-            // set up data structures
-            rawTypes = new HashSet<>();
-            rawMsgTypes.forEach(msg -> {
-                //log.info("raw type {}", msg);
-                rawTypes.add(msg);
-            });
-            cookedTypes = new HashSet<>();
-            cookedMsgTypes.forEach(msg -> {
-                //log.info("cooked type {}", msg);
-                cookedTypes.add(msg);
-            });
-            // find the message handler if it's not already there
-            setMessageAdapter();
-        }
-    }
-
-    private void setMessageAdapter()
-    {
-        if (null == adapter) {
-            adapter = (IpcAdapter) SpringApplicationContext.getBean(xmlForwardType);
-        }
-        if (null == adapter) {
-            log.error("Raw xml specified, but no adapter available");
-            rawXml = false;
-        }
-        log.info("Using {} for xml forwarding", xmlForwardType);
-    }
-
-
-    @Override
-    public void onMessage(Message message)
-    {
-        if (message instanceof TextMessage) {
-            String msg;
-            try {
-                log.debug("onMessage(Message) - receiving a message");
-                msg = ((TextMessage) message).getText();
-                log.info("received message:\n" + msg);
-                //onMessage(msg);
-                if (rawXml) {
-                    // Extract the tag, conditionally pass on the message and/or
-                    // unmarshal it and process it locally
-                    Matcher m = tagRe.matcher(msg);
-                    if (m.lookingAt()) {
-                        String tag = m.group(1);
-                        log.info("msg tag: {}", tag);
-                        if (cookedTypes.contains(tag)) {
-                            onMessage(msg);
-                        }
-                        if (rawTypes.contains(tag)) {
-                            adapter.exportMessage(msg);
-                        }
-                    }
-        }
-        else {
-                    onMessage(msg);
-                }
-            } catch (JMSException e) {
-                log.error("failed to extract text from TextMessage", e);
+          // Extract the tag, conditionally pass on the message and/or
+          // unmarshal it and process it locally
+          Matcher m = tagRe.matcher(msg);
+          if (m.lookingAt()) {
+            String tag = m.group(1);
+            log.info("msg tag: {}", tag);
+            if (cookedTypes.contains(tag)) {
+              onMessage(msg);
             }
+            if (rawTypes.contains(tag)) {
+              adapter.exportMessage(msg);
+            }
+          }
+        } else {
+          onMessage(msg);
         }
+      } catch (JMSException e) {
+        log.error("failed to extract text from TextMessage", e);
+      }
     }
+  }
 
-  private void onMessage (String xml) {
-        //log.info("onMessage(String) - received message:\n" + xml);
-        Object message = converter.fromXML(xml);
-        log.debug("onMessage(String) - received message of type " + message.getClass().getSimpleName());
-        messageDispatcher.routeMessage(message);
-    }
+  private void onMessage(String xml)
+  {
+    //log.info("onMessage(String) - received message:\n" + xml);
+    Object message = converter.fromXML(xml);
+    log.debug("onMessage(String) - received message of type " + message.getClass().getSimpleName());
+    messageDispatcher.routeMessage(message);
+  }
 }
