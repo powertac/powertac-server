47562349afdc7d5051d51bbd771359a8877b11a6
William Hulin <51363960+WilliamTYH@users.noreply.github.com>
2025-06-05 12:01:59 +0200

    Create missing "log" directory in StateLoggingTest

--- powertac-aop/src/test/java/org/powertac/common/state/StateLoggingTest.java
+++ powertac-aop/src/test/java/org/powertac/common/state/StateLoggingTest.java
@@ -1,15 +1,16 @@
 package org.powertac.common.state;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StateLoggingTest
 {
@@ -28,6 +29,11 @@ public class StateLoggingTest
     DummyDomain dd = new DummyDomain(1, "original");
     dd = new DummyDomain(31, 2, "second");
     dd.setNumber(42);
+
+    File logDir = new File("log");
+    if (!logDir.exists()) {
+      logDir.mkdirs();
+    }
 
     try (BufferedReader input = new BufferedReader(new FileReader("log/test.state")))
     {
@@ -70,6 +76,11 @@ public class StateLoggingTest
     DummyDomain dd = new DummyDomain(1, "abbreviated");
     dd = new DummyDomain(31, 2, "second");
     dd.setNumber(42);
+
+    File logDir = new File("log");
+    if (!logDir.exists()) {
+      logDir.mkdirs();
+    }
 
     try (BufferedReader input = new BufferedReader(new FileReader("log/test.state")))
     {
